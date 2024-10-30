ea2ea7cc03ee98e082c5f94c54161d035a870dd2
Dannick B. Senss <dannickbsenss@gmail.com>
2024-10-29 23:39:43 -0400

    ci: make ci fail

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const foo = 'foo';
+
+  return ( <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>Hello, world!</p>
+        <a>there</a>
         <a
           className="App-link"
           href="https://reactjs.org"
