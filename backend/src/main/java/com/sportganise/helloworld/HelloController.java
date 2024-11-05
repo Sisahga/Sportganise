69aede02bf3d8bfa9fdb9df7d5a503d352fa85fe
Sisahga Phimmasone <80082494+Sisahga@users.noreply.github.com>
2024-11-05 15:45:01 -0500

    Fixes (#220)
    * build: fixed package names and added proper postgresql config vars in application.properties
    
    Refs: #179
    
    * Update README.md

--- backend/src/main/java/com/sportganise/helloworld/HelloController.java
+++ backend/src/main/java/com/sportganise/helloworld/HelloController.java
@@ -1,4 +1,4 @@
-package com.sportsganize.helloworld;
+package com.sportganise.helloworld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
--- backend/src/test/java/com/sportganise/helloworld/HelloWorldApplicationTests.java
+++ backend/src/test/java/com/sportganise/helloworld/HelloWorldApplicationTests.java
@@ -1,4 +1,4 @@
-package com.sportsganize.helloworld;
+package com.sportganise.helloworld;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
