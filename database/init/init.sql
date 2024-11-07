000e6e9211688bb7c20d17f49a36048e18c03f51
Sisahga <sisahga@gmail.com>
2024-11-07 15:18:59 -0500

    chore: updated application.properties and docker-compose from pr #230.
    Refs: #205.

--- database/init/init.sql
+++ database/init/init.sql
@@ -12,7 +12,7 @@ CREATE TABLE users (
   u_id SERIAL PRIMARY KEY,
   type VARCHAR(30) NOT NULL,
   email VARCHAR(254) UNIQUE NOT NULL,
-  password VARCHAR(255) NOT NULL,
+  auth0_user_id VARCHAR(255) UNIQUE NOT NULL,
   address VARCHAR(100),
   phone VARCHAR(15) NOT NULL,
   first_name VARCHAR(50) NOT NULL,
