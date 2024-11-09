5c586c1b6a342dd7bac234a70fb2d879c8081a46
Dannick B. Senss <dannickbsenss@gmail.com>
2024-11-09 16:11:26 -0500

    Update tests

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -20,6 +20,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <p>Welcome to our app!</p>
         <img src={logo} className="App-logo" alt="logo" />
         <p>{homeText}</p>
       </header>
