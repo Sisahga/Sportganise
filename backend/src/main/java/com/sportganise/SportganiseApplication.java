9b708f8ae3c575e0bc4c7c08edb2ecda914ca520
Sisahga <sisahga@gmail.com>
2024-11-11 19:55:23 -0500

    WIP: created main sportganiseapplication.java file as opposed to the initial helloworld version.
    \nRefs: #249.

---
+++ backend/src/main/java/com/sportganise/SportganiseApplication.java
@@ -0,0 +1,11 @@
+package com.sportganise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportganiseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SportganiseApplication.class, args);
+    }
+}
