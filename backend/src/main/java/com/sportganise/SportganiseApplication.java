6ff05464fbb9a64f59b0e29c0bdd3f65c34bbbb6
Sisahga <sisahga@gmail.com>
2024-11-19 19:21:39 -0500

    fix: linting.

--- backend/src/main/java/com/sportganise/SportganiseApplication.java
+++ backend/src/main/java/com/sportganise/SportganiseApplication.java
@@ -3,6 +3,9 @@ package com.sportganise;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * This is our Sportganise SpringBoot application.
+ */
 @SpringBootApplication
 public class SportganiseApplication {
   public static void main(String[] args) {
--- backend/src/main/java/com/sportganise/services/AccountServiceImpl.java
+++ backend/src/main/java/com/sportganise/services/AccountServiceImpl.java
@@ -6,7 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/** Implementation of AccountService */
+/** Implementation of AccountService. */
 @Service
 public class AccountServiceImpl implements AccountService {
   private final AccountRepository accountRepository;
