f0f3d99cae94c32c775bce96038ab6b8a3ee6024
Sisahga <sisahga@gmail.com>
2024-11-21 15:44:32 -0500

    style: removed useless import.
    Refs: #143.

--- backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageChannelRepository.java
+++ backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageChannelRepository.java
@@ -3,7 +3,5 @@ package com.sportganise.repositories.directmessaging;
 import com.sportganise.entities.directmessaging.DirectMessageChannel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface DirectMessageChannelRepository extends JpaRepository<DirectMessageChannel, Integer> {
 }
