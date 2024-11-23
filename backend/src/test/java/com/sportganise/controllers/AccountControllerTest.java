7a0b1850828bbc7e37f2862e8afc4c15538d948b
karyennevu <vuongkaryenne@gmail.com>
2024-11-22 20:59:43 -0500

    Fix spotless format

--- backend/src/test/java/com/sportganise/controllers/AccountControllerTest.java
+++ backend/src/test/java/com/sportganise/controllers/AccountControllerTest.java
@@ -1,18 +1,18 @@
-//package com.sportganise.controllers;
+// package com.sportganise.controllers;
 //
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.hamcrest.Matchers.not;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.hamcrest.Matchers.equalTo;
+// import static org.hamcrest.Matchers.not;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 //
-//import org.junit.jupiter.api.Order;
-//import org.junit.jupiter.api.Test;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+// import org.junit.jupiter.api.Order;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.http.MediaType;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /*
 @SpringBootTest
--- backend/src/test/java/com/sportganise/controllers/ProgramControllerTest.java
+++ backend/src/test/java/com/sportganise/controllers/ProgramControllerTest.java
@@ -1,27 +1,27 @@
-//package com.sportganise.controllers;
+// package com.sportganise.controllers;
 //
-//import static org.hamcrest.Matchers.equalTo;
-//import static org.hamcrest.Matchers.hasSize;
-//import static org.hamcrest.Matchers.is;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+// import static org.hamcrest.Matchers.equalTo;
+// import static org.hamcrest.Matchers.hasSize;
+// import static org.hamcrest.Matchers.is;
+// import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 //
-//import com.sportganise.dto.ProgramParticipantDto;
-//import com.sportganise.entities.Account;
-//import com.sportganise.entities.Role;
-//import com.sportganise.services.AccountService;
-//import com.sportganise.services.ProgramService;
-//import java.util.Arrays;
-//import java.util.Optional;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.Mockito;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-//import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.boot.test.mock.mockito.MockBean;
-//import org.springframework.http.MediaType;
-//import org.springframework.test.context.ActiveProfiles;
-//import org.springframework.test.web.servlet.MockMvc;
-//import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+// import com.sportganise.dto.ProgramParticipantDto;
+// import com.sportganise.entities.Account;
+// import com.sportganise.entities.Role;
+// import com.sportganise.services.AccountService;
+// import com.sportganise.services.ProgramService;
+// import java.util.Arrays;
+// import java.util.Optional;
+// import org.junit.jupiter.api.Test;
+// import org.mockito.Mockito;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+// import org.springframework.boot.test.context.SpringBootTest;
+// import org.springframework.boot.test.mock.mockito.MockBean;
+// import org.springframework.http.MediaType;
+// import org.springframework.test.context.ActiveProfiles;
+// import org.springframework.test.web.servlet.MockMvc;
+// import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 /*
 @SpringBootTest
