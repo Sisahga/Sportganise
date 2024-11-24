369fb9a495c6cbdde2dfb01744712f2bd9faa5c1
Sisahga <sisahga@gmail.com>
2024-11-24 02:13:32 -0500

    chore: fix sportganise app test.

--- backend/src/test/java/com/sportganise/SportganiseApplicationTest.java
+++ backend/src/test/java/com/sportganise/SportganiseApplicationTest.java
@@ -2,8 +2,10 @@ package com.sportganise;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SportganiseApplicationTest {
   @Test
   void contextLoads() {}
