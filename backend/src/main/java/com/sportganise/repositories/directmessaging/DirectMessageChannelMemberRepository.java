d53a6e4a9448321929f0411c91c034b509581789
Sisahga <sisahga@gmail.com>
2024-11-24 15:30:54 -0500

    wip: added query to delete all channel_member related to a direct message channel upon deletion.

--- backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageChannelMemberRepository.java
+++ backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageChannelMemberRepository.java
@@ -2,8 +2,18 @@ package com.sportganise.repositories.directmessaging;
 
 import com.sportganise.entities.directmessaging.DirectMessageChannelMember;
 import com.sportganise.entities.directmessaging.DirectMessageChannelMemberCompositeKey;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 /** Repository for a Direct Channel Member. */
 public interface DirectMessageChannelMemberRepository
-    extends JpaRepository<DirectMessageChannelMember, DirectMessageChannelMemberCompositeKey> {}
+    extends JpaRepository<DirectMessageChannelMember, DirectMessageChannelMemberCompositeKey> {
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM DirectMessageChannelMember " +
+            "WHERE DirectMessageChannelMember.compositeKey.channelId = :channelId")
+    void deleteDirectMessageChannelMemberByChannelId(@Param("channelId") Integer channelId);
+}
