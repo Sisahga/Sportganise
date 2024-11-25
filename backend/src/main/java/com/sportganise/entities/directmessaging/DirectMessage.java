79f054bc724a8dda59d0c290bdbcb5990c0a3ff4
Sisahga <sisahga@gmail.com>
2024-11-24 21:28:35 -0500

    chore: getters, setters, and constructors for DirectMessage.java
    Refs: #136.

--- backend/src/main/java/com/sportganise/entities/directmessaging/DirectMessage.java
+++ backend/src/main/java/com/sportganise/entities/directmessaging/DirectMessage.java
@@ -6,9 +6,18 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.time.LocalDateTime;
 
 /** Entity for message. */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 @Entity
 @Table(name = "message")
 public class DirectMessage {
