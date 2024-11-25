068be5c3cb3ec2e217f8d170715b2cc65d718f6b
Sisahga <sisahga@gmail.com>
2024-11-25 13:48:56 -0500

    wip: added query to get the necessary fields for sending the DTO for all dm channels related to a specific account.
    Refs: #18 #136.

--- backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageChannelRepository.java
+++ backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageChannelRepository.java
@@ -2,7 +2,25 @@ package com.sportganise.repositories.directmessaging;
 
 import com.sportganise.entities.directmessaging.DirectMessageChannel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 /** Repository for Direct Message Channel. */
 public interface DirectMessageChannelRepository
-    extends JpaRepository<DirectMessageChannel, Integer> {}
+    extends JpaRepository<DirectMessageChannel, Integer> {
+    /**
+     * Query that gets DTO object for all Direct Message Channels for an account.
+     *
+     * @param accountId Id of the account to get Direct Message Channels for.
+     */
+    @Query("""
+            SELECT c.channelId, c.name as channelName, c.type as channelType, c.imageBlob as channelImageBlob,
+                        cm.read, m.content as lastMessage, m.sentAt as lastEvent
+            FROM DirectMessageChannel c
+            INNER JOIN DirectMessageChannelMember cm ON c.channelId = cm.compositeKey.channelId
+            LEFT JOIN DirectMessage m ON c.channelId = m.channelId AND m.sentAt = (
+                    SELECT MAX(sentAt) FROM DirectMessage WHERE channelId = c.channelId
+                )
+            WHERE cm.compositeKey.accountId = :accountId
+            """)
+    void getDirectMessageChannelsByAccountId(int accountId);
+}
