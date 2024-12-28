9bc1fc087a93a53bf55fca8be3d6494c32d9e2de
Sofia Valiante <valiante.sofia@gmail.com>
2024-12-27 20:00:03 -0500

    feat(303): datepicker start-date form field

--- frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
+++ frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
@@ -241,6 +241,53 @@ export default function CreateTrainingSessionForm() {
             </FormItem>
           )}
         />
+
+        {/** Start Date */}
+        <FormField
+          control={form.control}
+          name="start_day"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel className="font-semibold text-base">
+                Start Date
+              </FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Enter the first date of the event. Applies for recurring and non
+                recurring events. If recurring, this day will be the assumed
+                repeat day in the future.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </form>
     </Form>
   );
