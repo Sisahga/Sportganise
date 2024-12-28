bb3526c9670e45d98b584182b103737f0acd9652
Sofia Valiante <valiante.sofia@gmail.com>
2024-12-27 20:02:26 -0500

    feat(303): datepicker end-date form field

--- frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
+++ frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
@@ -288,6 +288,51 @@ export default function CreateTrainingSessionForm() {
             </FormItem>
           )}
         />
+
+        {/** End Date */}
+        <FormField
+          control={form.control}
+          name="end_date"
+          render={({ field }) => (
+            <FormItem className="flex flex-col">
+              <FormLabel className="font-semibold text-base">
+                End Date
+              </FormLabel>
+              <Popover>
+                <PopoverTrigger asChild>
+                  <FormControl>
+                    <Button
+                      variant={"outline"}
+                      className={cn(
+                        "pl-3 text-left font-normal",
+                        !field.value && "text-muted-foreground"
+                      )}
+                    >
+                      {field.value ? (
+                        format(field.value, "PPP")
+                      ) : (
+                        <span>Pick a date</span>
+                      )}
+                      <CalendarIcon className="ml-auto h-4 w-4 opacity-50" />
+                    </Button>
+                  </FormControl>
+                </PopoverTrigger>
+                <PopoverContent className="w-auto p-0" align="start">
+                  <Calendar
+                    mode="single"
+                    selected={field.value}
+                    onSelect={field.onChange}
+                    initialFocus
+                  />
+                </PopoverContent>
+              </Popover>
+              <FormDescription>
+                Enter the last day of a recurring event.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </form>
     </Form>
   );
