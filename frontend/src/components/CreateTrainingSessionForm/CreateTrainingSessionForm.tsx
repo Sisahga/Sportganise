c7c1898341df7f2f5d38fb871552ae9b3735816c
Sofia Valiante <valiante.sofia@gmail.com>
2024-12-27 20:06:14 -0500

    feat(303): 'file-upload' form field

--- frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
+++ frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
@@ -445,6 +445,58 @@ export default function CreateTrainingSessionForm() {
             </FormItem>
           )}
         />
+
+        {/** Add Attachment */}
+        <FormField
+          control={form.control}
+          name="attachment"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel className="font-semibold text-base">
+                Add Attachment
+              </FormLabel>
+              <FormControl>
+                <FileUploader
+                  value={files}
+                  onValueChange={(newFiles) => {
+                    setFiles(newFiles); // Update local state
+                    field.onChange(newFiles); // Sync with React Hook Form
+                  }}
+                  dropzoneOptions={dropZoneConfig}
+                  className="relative bg-background rounded-lg p-2"
+                >
+                  <FileInput
+                    id="fileInput"
+                    className="outline-dashed outline-1 outline-slate-500"
+                  >
+                    <div className="flex items-center justify-center flex-col p-8 w-full ">
+                      <CloudUpload className="text-gray-500 w-10 h-10" />
+                      <p className="mb-1 text-sm text-gray-500 dark:text-gray-400">
+                        <span className="font-semibold">Click to upload</span>
+                        &nbsp; or drag and drop
+                      </p>
+                      <p className="text-xs text-gray-500 dark:text-gray-400">
+                        PNG, JPG, PDF and more
+                      </p>
+                    </div>
+                  </FileInput>
+                  <FileUploaderContent>
+                    {files &&
+                      files.length > 0 &&
+                      files.map((file, i) => (
+                        <FileUploaderItem key={i} index={i}>
+                          <Paperclip className="h-4 w-4 stroke-current" />
+                          <span>{file.name}</span>
+                        </FileUploaderItem>
+                      ))}
+                  </FileUploaderContent>
+                </FileUploader>
+              </FormControl>
+              <FormDescription>Select a file to upload.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
       </form>
     </Form>
   );
