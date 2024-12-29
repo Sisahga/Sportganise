bf276ac5a9d72cd5c8d1c98666a653f5555bc733
Vithu <vithujanan0629@gmail.com>
2024-12-29 17:24:22 -0500

    Redirection from sign up page to verification page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
-import { SignUp } from "./components/SignUp";
+import SignUpPage from "./pages/SignUpPage";
+import VerificationCodePage from "./pages/VerificationCodePage";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUp />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/verificationcode" element={<VerificationCodePage />} />
       </Routes>
     </Router>
   );
