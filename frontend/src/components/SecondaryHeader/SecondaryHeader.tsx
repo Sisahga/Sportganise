96a4f88b2e29dafaaea88f6b14d703db94159602
Vithu <vithujanan0629@gmail.com>
2024-12-29 17:15:34 -0500

    When clicking on the left arrow, you can now go back to previous page.

--- frontend/src/components/SecondaryHeader/SecondaryHeader.tsx
+++ frontend/src/components/SecondaryHeader/SecondaryHeader.tsx
@@ -1,12 +1,23 @@
 import { Button } from "@/components/ui/Button";
 import logo from "../../assets/Logo.png";
 import { ArrowLeft } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 export default function SecondaryHeader() {
+  const navigate = useNavigate();
+
+  // Function to handle navigation to previous page using the history 
+  const handleGoBack = () => {
+    navigate(-1); //Brings you to previous page
+  };
+
   return (
     <div>
       <header className="fixed top-0 left-0 right-0 z-40 p-4 flex items-center justify-between bg-white">
-        <Button className="flex items-center justify-center w-12 h-12 rounded-full bg-white text-black border-black hover:bg-secondaryColour">
+        <Button
+          className="flex items-center justify-center w-12 h-12 rounded-full bg-white text-black border-black hover:bg-secondaryColour"
+          onClick={handleGoBack}
+        >
           <ArrowLeft className="w-8 h-8" />
         </Button>
 
