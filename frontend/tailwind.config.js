3b8e03fb61b780899e8fc9f16d1326f32f657bb0
Sisahga <sisahga@gmail.com>
2024-12-30 21:21:10 -0500

    fix: properly configured tailwind.config.js file so that frontend can run

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,4 +1,5 @@
 /** @type {import('tailwindcss').Config} */
+import animate from "tailwindcss-animate";
 export default {
   darkMode: ["class"],
   content: ["./index.html", "./src/**/*.{ts,tsx,js,jsx}"],
@@ -71,5 +72,5 @@ export default {
       },
     },
   },
-  plugins: [require("tailwindcss-animate")],
+  plugins: [animate],
 };
