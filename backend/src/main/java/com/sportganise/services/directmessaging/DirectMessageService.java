80fba3cd556ad91a1b59b7be1e42c3849467e2ee
Sisahga <sisahga@gmail.com>
2024-12-30 21:34:53 -0500

    feat: Updated DirectMessageService SendDirectMessageDto.java method to handle attachments.
    Refs: #17 #196 #200

--- backend/src/main/java/com/sportganise/services/directmessaging/DirectMessageService.java
+++ backend/src/main/java/com/sportganise/services/directmessaging/DirectMessageService.java
@@ -1,43 +1,50 @@
 package com.sportganise.services.directmessaging;
 
-import com.sportganise.dto.directmessaging.SendDirectMessageDto;
+import com.sportganise.dto.directmessaging.SendDirectMessageRequestDto;
+import com.sportganise.dto.directmessaging.SendDirectMessageResponseDto;
 import com.sportganise.entities.directmessaging.DirectMessage;
 import com.sportganise.repositories.directmessaging.DirectMessageChannelMemberRepository;
 import com.sportganise.repositories.directmessaging.DirectMessageChannelRepository;
 import com.sportganise.repositories.directmessaging.DirectMessageRepository;
+import com.sportganise.services.BlobService;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Service
 public class DirectMessageService {
     private final DirectMessageRepository directMessageRepository;
     private final DirectMessageChannelRepository directMessageChannelRepository;
     private final DirectMessageChannelMemberRepository directMessageChannelMemberRepository;
+    private final BlobService blobService;
 
     public DirectMessageService(DirectMessageRepository directMessageRepository,
                                 DirectMessageChannelRepository directMessageChannelRepository,
-                                DirectMessageChannelMemberRepository directMessageChannelMemberRepository) {
+                                DirectMessageChannelMemberRepository directMessageChannelMemberRepository,
+                                BlobService blobService) {
         this.directMessageRepository = directMessageRepository;
         this.directMessageChannelRepository = directMessageChannelRepository;
         this.directMessageChannelMemberRepository = directMessageChannelMemberRepository;
+        this.blobService = blobService;
     }
 
     /**
      * Sends a Direct Message and stores it in the Database while updating related tables.
      *
-     * @param sendDirectMessageDto DTO containing the message details.
+     * @param sendDirectMessageRequestDto DTO containing the message details.
      * @return DTO containing information about the sent message.
      */
-    public SendDirectMessageDto sendDirectMessage(SendDirectMessageDto sendDirectMessageDto) {
-        int channelId = sendDirectMessageDto.getChannelId();
-        int senderId = sendDirectMessageDto.getSenderId();
+    public SendDirectMessageResponseDto sendDirectMessage(
+            SendDirectMessageRequestDto sendDirectMessageRequestDto) throws IOException {
+        int channelId = sendDirectMessageRequestDto.getChannelId();
+        int senderId = sendDirectMessageRequestDto.getSenderId();
         DirectMessage directMessage = new DirectMessage();
         directMessage.setSenderId(senderId);
         directMessage.setChannelId(channelId);
-        directMessage.setContent(sendDirectMessageDto.getMessageContent());
-        directMessage.setSentAt(LocalDateTime.parse(sendDirectMessageDto.getSentAt()));
+        directMessage.setContent(sendDirectMessageRequestDto.getMessageContent());
+        directMessage.setSentAt(LocalDateTime.parse(sendDirectMessageRequestDto.getSentAt()));
         directMessageRepository.save(directMessage);
 
         // Update Last Message in Channel Table.
@@ -46,10 +53,23 @@ public class DirectMessageService {
         // Update Read Status in Channel Member Table.
         directMessageChannelMemberRepository.updateReadStatus(channelId, senderId);
 
-        // TODO: Create Attachment Entity, Repository, and Service.
-        // TODO: Create Attachment table and Store attachments with Attachment Service.
+        SendDirectMessageResponseDto sendDirectMessageResponseDto = new SendDirectMessageResponseDto();
+        sendDirectMessageResponseDto.setSenderId(senderId);
+        sendDirectMessageResponseDto.setChannelId(channelId);
+        sendDirectMessageResponseDto.setMessageContent(sendDirectMessageRequestDto.getMessageContent());
+        sendDirectMessageResponseDto.setSentAt(sendDirectMessageRequestDto.getSentAt());
+
+        // Upload Attachments to Blob Storage and Persist Message-Attachment relationship in DB.
+        String[] attachments = new String[sendDirectMessageRequestDto.getAttachments().length];
+        for (int i = 0; i < sendDirectMessageRequestDto.getAttachments().length; i++) {
+            MultipartFile file = sendDirectMessageRequestDto.getAttachments()[i];
+            String blobUrl = blobService.uploadFile(file, true, directMessage.getMessageId().toString());
+            attachments[i] = blobUrl;
+        }
+        sendDirectMessageResponseDto.setAttachments(attachments);
+
         // TODO: Implement Nofication Service.
 
-        return sendDirectMessageDto;
+        return sendDirectMessageResponseDto;
     }
 }
