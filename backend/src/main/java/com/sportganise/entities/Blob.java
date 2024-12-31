b9c7530b481821611af0faf687ffdb4c62c8215b
Sisahga <sisahga@gmail.com>
2024-12-30 21:29:51 -0500

    feat: added blob entity.

---
+++ backend/src/main/java/com/sportganise/entities/Blob.java
@@ -0,0 +1,27 @@
+package com.sportganise.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+public class Blob {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "blob_id")
+    private Integer blobId;
+    @Column(name = "blob_url")
+    private String blobUrl;
+}
