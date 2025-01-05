d83a5e20969da8bdb6c665d3d5b683b12a25b24b
Sisahga <sisahga@gmail.com>
2025-01-05 15:53:34 -0500

    fix: switched from MultipartFile array to a List of type MultipartFile.

--- backend/src/main/java/com/sportganise/dto/directmessaging/SendDirectMessageRequestDto.java
+++ backend/src/main/java/com/sportganise/dto/directmessaging/SendDirectMessageRequestDto.java
@@ -1,19 +1,22 @@
 package com.sportganise.dto.directmessaging;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+/** DTO for accepting a direct message request. */
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class SendDirectMessageRequestDto {
-    private Integer senderId;
-    private Integer channelId;
-    private String messageContent;
-    private MultipartFile[] attachments;
-    private String sentAt;
+  private Integer senderId;
+  private Integer channelId;
+  private String messageContent;
+  private List<MultipartFile> attachments;
+  private String sentAt;
+  private String type;
 }
