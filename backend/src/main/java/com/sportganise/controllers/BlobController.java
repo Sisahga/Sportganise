82ac6fb0f43b123eb2b2adaa651a8224f51e8b71
Sisahga <sisahga@gmail.com>
2025-01-05 15:48:41 -0500

    feat: added BlobServiceController class to handle uploading file requests.

--- backend/src/main/java/com/sportganise/controllers/BlobController.java
+++ backend/src/main/java/com/sportganise/controllers/BlobController.java
@@ -1,6 +1,7 @@
 package com.sportganise.controllers;
 
 import com.sportganise.services.BlobService;
+import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -10,27 +11,36 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
-
+/** Controller for handling blob storage operations. */
 @RestController
 @RequestMapping("/api/blob")
 @Slf4j
 public class BlobController {
-    private final BlobService blobService;
-    public BlobController(BlobService blobService) {
-        this.blobService = blobService;
-    }
+  private final BlobService blobService;
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile file,
-                                             @RequestParam("isMessageFile") boolean isMessageFile,
-                                             @RequestParam("messageId") String messageId) {
-        try {
-            String status = blobService.uploadFile(file, isMessageFile, messageId);
-            return new ResponseEntity<>(status, HttpStatus.OK);
-        } catch (IOException e) {
-            log.error("Error uploading file: {}", e.getMessage());
-            return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+  public BlobController(BlobService blobService) {
+    this.blobService = blobService;
+  }
+
+  /**
+   * Uploads a file to blob storage.
+   *
+   * @param file the file to upload
+   * @param isMessageFile whether the file to store is an attachment to a direct message
+   * @param messageId the message ID of the direct message the file is attached to
+   * @return the status of the upload
+   */
+  @PostMapping("/upload")
+  public ResponseEntity<String> uploadFile(
+      @RequestParam("file") MultipartFile file,
+      @RequestParam("isMessageFile") boolean isMessageFile,
+      @RequestParam("messageId") String messageId) {
+    try {
+      String status = blobService.uploadFile(file, isMessageFile, messageId);
+      return new ResponseEntity<>(status, HttpStatus.OK);
+    } catch (IOException e) {
+      log.error("Error uploading file: {}", e.getMessage());
+      return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
     }
+  }
 }
