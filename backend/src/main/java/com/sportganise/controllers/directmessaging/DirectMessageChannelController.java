f7e1f8920c758a3f690faea06daea67d70cc729a
Sisahga <sisahga@gmail.com>
2025-01-05 18:53:14 -0500

    fix: added /channel in the request mapping, refactored out DirectMessage related methods to its own controller class.

--- backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageChannelController.java
+++ backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageChannelController.java
@@ -2,24 +2,12 @@ package com.sportganise.controllers.directmessaging;
 
 import com.sportganise.dto.directmessaging.CreateDirectMessageChannelDto;
 import com.sportganise.dto.directmessaging.ListDirectMessageChannelDto;
-import com.sportganise.dto.directmessaging.SendDirectMessageRequestDto;
-import com.sportganise.dto.directmessaging.SendDirectMessageResponseDto;
-import com.sportganise.entities.Account;
-import com.sportganise.repositories.AccountRepository;
-import com.sportganise.repositories.directmessaging.DirectMessageChannelMemberRepository;
 import com.sportganise.services.directmessaging.DirectMessageChannelService;
-import com.sportganise.services.directmessaging.DirectMessageService;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.messaging.handler.annotation.MessageMapping;
-import org.springframework.messaging.handler.annotation.Payload;
-import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -31,14 +19,11 @@ import org.springframework.web.bind.annotation.RestController;
 
 /** REST Controller for handling HTTP requests related to Direct Message Channels. */
 @RestController
-@RequestMapping("/api/messaging")
+@RequestMapping("/api/messaging/channel")
 @Slf4j
 @CrossOrigin("*")
 public class DirectMessageChannelController {
   private final DirectMessageChannelService directMessageChannelService;
-  private final DirectMessageService directMessageService;
-  private final DirectMessageChannelMemberRepository directMessageChannelMemberRepository;
-  private final AccountRepository accountRepository;
 
   /**
    * Controller Constructor.
@@ -47,61 +32,8 @@ public class DirectMessageChannelController {
    */
   @Autowired
   public DirectMessageChannelController(
-      DirectMessageChannelService directMessageChannelService,
-      DirectMessageService directMessageService,
-      DirectMessageChannelMemberRepository directMessageChannelMemberRepository,
-      AccountRepository accountRepository) {
+      DirectMessageChannelService directMessageChannelService) {
     this.directMessageChannelService = directMessageChannelService;
-    this.directMessageService = directMessageService;
-    this.directMessageChannelMemberRepository = directMessageChannelMemberRepository;
-    this.accountRepository = accountRepository;
-  }
-
-  /**
-   * Send a direct message.
-   *
-   * @param messageDto Direct message request DTO for sending a message.
-   * @return Response DTO for sending a message.
-   */
-  @MessageMapping("/chat.send-message")
-  @SendTo("/directmessage/public")
-  public SendDirectMessageResponseDto sendDirectMessage(
-      @Payload SendDirectMessageRequestDto messageDto) {
-    try {
-      log.info("Controller received message request.");
-      return directMessageService.sendDirectMessage(messageDto);
-    } catch (Exception e) {
-      log.error("Error sending message", e);
-      return null;
-    }
-  }
-
-  /**
-   * Notifies a channel that a member is active in the channel.
-   *
-   * @param messageDto Direct message request DTO for adding a member.
-   * @param headerAccessor SimpMessageHeaderAccessor for the web socket session.
-   * @return Response DTO for adding a member.
-   */
-  // TODO (In Progress, next sprint): Create association between new member and channel in database.
-  @MessageMapping("/chat.add-member")
-  @SendTo("/directmessage/public")
-  public SendDirectMessageResponseDto addMember(
-      @Payload SendDirectMessageRequestDto messageDto, SimpMessageHeaderAccessor headerAccessor) {
-    try {
-      SendDirectMessageResponseDto responseDto = directMessageService.addMember(messageDto);
-      // Expect content to be id of new user when adding a new user to a message channel.
-      int newUserId = Integer.parseInt(messageDto.getMessageContent());
-      Optional<Account> newMember = accountRepository.findById(newUserId);
-      // Add username in web socket session
-      newMember.ifPresent(
-          account ->
-              Objects.requireNonNull(headerAccessor.getSessionAttributes())
-                  .put("username", account.getFirstName()));
-      return responseDto;
-    } catch (Exception e) {
-      return null;
-    }
   }
 
   /**
