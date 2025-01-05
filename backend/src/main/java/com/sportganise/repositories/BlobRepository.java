3e752f139bb2b77dc9ea796bbd7094be18e2bd51
Sisahga <sisahga@gmail.com>
2025-01-05 15:46:51 -0500

    feat: added BlobRepository for the blob entity.

--- backend/src/main/java/com/sportganise/repositories/BlobRepository.java
+++ backend/src/main/java/com/sportganise/repositories/BlobRepository.java
@@ -4,6 +4,6 @@ import com.sportganise.entities.Blob;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+/** Repository for the Blob entity. */
 @Repository
-public interface BlobRepository extends JpaRepository<Blob, Integer> {
-}
+public interface BlobRepository extends JpaRepository<Blob, Integer> {}
