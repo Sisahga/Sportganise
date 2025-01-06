53e36ebb589fe27b89eef17231d2a26cc6f8e5fe
Sisahga <sisahga@gmail.com>
2025-01-05 20:35:04 -0500

    test: fix SportganiseApplicationTest.

--- backend/src/test/java/com/sportganise/SportganiseApplicationTest.java
+++ backend/src/test/java/com/sportganise/SportganiseApplicationTest.java
@@ -3,10 +3,18 @@ package com.sportganise;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import javax.sql.DataSource;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class SportganiseApplicationTest {
+
+  @MockBean
+  private DataSource dataSource;
+
   @Test
   void contextLoads() {}
 }
