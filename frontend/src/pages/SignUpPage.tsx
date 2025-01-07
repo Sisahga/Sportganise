f4a0fe7c5bbea46f242cfc2e2a22f1c2f62ef31e
Vithujanan Vigneswaran <92345647+Houdini29@users.noreply.github.com>
2025-01-07 15:04:51 -0500

    feat(103): Created the SignUp page (#391)
    * Created a reusable form field to avoid repetitve code for the sign up page
    
    * Reusasble header with the logo and back button for the sign up and login page
    
    * Link to get to the sign up page
    
    * Created the sign up page
    
    * Setting up the SignUp page
    
    * Created the VerificationCode page
    
    * Routed the SignUpPage and VerificationCodePage
    
    * Reformat using prettier
    
    * Reformat with prettier

---
+++ frontend/src/pages/SignUpPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { SignUp } from "../components/SignUp/index";
+
+const SignUpPage: React.FC = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <SignUp />
+    </div>
+  );
+};
+
+export default SignUpPage;
