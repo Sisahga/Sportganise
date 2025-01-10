711dba813b9e51426ed0ebecb0c63551461cdd37
karyennevu <vuongkaryenne@gmail.com>
2025-01-10 03:35:16 -0500

    Removed uneccessary imports

--- backend/src/test/java/com/sportganise/controllers/programsession/ProgramControllerTest.java
+++ backend/src/test/java/com/sportganise/controllers/programsession/ProgramControllerTest.java
@@ -24,11 +24,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.multipart.MultipartFile;
 
 @WebMvcTest(controllers = ProgramController.class)
 @AutoConfigureMockMvc
