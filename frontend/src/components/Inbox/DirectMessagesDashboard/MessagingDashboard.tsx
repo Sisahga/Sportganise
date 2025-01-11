973a56c48944d28e7e5efd36fe7e9da268d230b2
Sisahga <sisahga@gmail.com>
2025-01-11 18:09:39 -0500

    feat: integrated viewing all channels related to a user with full-stack functionality. Refs: #17 #18

---
+++ frontend/src/components/Inbox/DirectMessagesDashboard/MessagingDashboard.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useState } from "react";
+import GroupSection from "@/components/Inbox/GroupMessages/GroupSection";
+import MessagesSection from "../SimpleMessages/MessagesSection.tsx";
+import { Channel } from "@/types/dmchannels.ts";
+import directMessagingApi from "@/services/api/directMessagingApi.ts";
+import "./MessagingDashboard.css";
+import MessagingDashboardHeader from "@/components/Inbox/DirectMessagesDashboard/MessagingDashboardHeader.tsx";
+
+function DirectMessagesDashboard() {
+  const accountId = 2;
+  const [channels, setChannels] = useState<Channel[]>([]);
+  const [error, setError] = useState<string | null>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const simpleChannels = channels.filter(channel => channel.channelType === "SIMPLE");
+  const groupChannels = channels.filter(channel => channel.channelType === "GROUP");
+
+  const fetchChannels = async () => {
+    try {
+      const response = await directMessagingApi.getChannels(accountId)
+      setChannels(response);
+    } catch (err) {
+      console.error("Error fetching chat messages:", err);
+      setError("Failed to load messages.");
+    }
+  };
+
+  useEffect(() => {
+    fetchChannels().then(() => setLoading(false));
+  }, []);
+
+  useEffect(() => {
+    console.log("Simple Channels: ", simpleChannels);
+  }, [simpleChannels]);
+  useEffect(() => {
+    console.log("Group Channels: ", groupChannels);
+  }, [groupChannels]);
+
+  if (loading) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p>Loading...</p>
+      </div>
+    );
+  }
+
+  if (error) {
+    return (
+      <div className="flex items-center justify-center h-screen">
+        <p className="text-red-500">{error}</p>
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col h-screen bg-gray-100">
+      <MessagingDashboardHeader />
+      <GroupSection groupChannels={groupChannels} />
+      <MessagesSection messageChannels={simpleChannels} />
+    </div>
+  );
+}
+
+export default DirectMessagesDashboard;
