2ded70ccb1466c0032822afa70fb52ca5c81e21b
Sisahga <sisahga@gmail.com>
2025-01-11 17:55:40 -0500

    feat: added controller mapping to update a channel member's read status. Refs: #17 #18

---
+++ backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageChannelMemberController.java
@@ -0,0 +1,40 @@
+package com.sportganise.controllers.directmessaging;
+
+import com.sportganise.services.directmessaging.DirectMessageChannelMemberService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Controller for Direct Message Channel Members.
+ */
+@RestController
+@RequestMapping("/api/messaging/channelmember")
+@Slf4j
+public class DirectMessageChannelMemberController {
+  private final DirectMessageChannelMemberService directMessageChannelMemberService;
+
+  public DirectMessageChannelMemberController(DirectMessageChannelMemberService directMessageChannelMemberService) {
+    this.directMessageChannelMemberService = directMessageChannelMemberService;
+  }
+
+  /**
+   * Marks a channel as read for a specific account.
+   * @param channelId The channel id.
+   * @param accountId The account id.
+   * @return ResponseEntity with status 200 if successful, 404 if not.
+   */
+  @PutMapping("/{channelId}/{accountId}/mark-as-read")
+  public ResponseEntity<Void> markChannelAsRead(@PathVariable int channelId, @PathVariable int accountId) {
+    int updatedRows = this.directMessageChannelMemberService.markChannelAsRead(channelId, accountId);
+    if (updatedRows == 0) {
+      log.error("Failed to mark channel as read for channel id: {} and account id: {}", channelId, accountId);
+      return ResponseEntity.notFound().build();
+    }
+    log.info("Successfully marked channel as read for channel id: {} and account id: {}", channelId, accountId);
+    return ResponseEntity.ok().build();
+  }
+}
