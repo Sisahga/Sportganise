538191bc1fd7445731ba0d37e4f7df465ffb514a
Sisahga <sisahga@gmail.com>
2025-01-11 18:03:54 -0500

    feat: added services folder and added an api folder for all api fetches. Refs: #17 #18 #196

--- frontend/src/services/api/directMessagingApi.ts
+++ frontend/src/services/api/directMessagingApi.ts
@@ -1,15 +1,24 @@
-import sendMessageRequestDto from '../../types/messaging.ts';
+import { Channel } from '@/types/dmchannels.ts';
+import { MessageComponent } from "@/types/messaging.ts";
 
-const baseMappingUrl = process.env.REACT_APP_API_BASE_URL + "/app/messaging/directmessage";
+const baseMappingUrl = import.meta.env.VITE_API_BASE_URL + "/api/messaging";
 
 const directMessagingApi = {
-    sendDirectMessage: async (message: sendMessageRequestDto) => {
-        const response = await fetch(baseMappingUrl + "/", {
-        method: 'POST',
-        body: JSON.stringify({ message }),
-        });
-        return response.json();
+    getDirectMessages: async (channelId: number | null) => {
+        const response = await fetch(`${baseMappingUrl}/directmessage/get-messages/${channelId}`);
+        const data:MessageComponent[] = await response.json();
+        return data;
     },
+    getChannels: async (accountId: number | null) => {
+        const response = await fetch(`${baseMappingUrl}/channel/get-channels/${accountId}`);
+        const data:Channel[]  = await response.json();
+        return data;
+    },
+    markChannelAsRead: async (channelId: number, userId: number): Promise<void> => {
+        await fetch(`${baseMappingUrl}/channelmember/${channelId}/${userId}/mark-as-read`, {
+            method: "PUT",
+        });
+    }
 }
 
 export default directMessagingApi;
