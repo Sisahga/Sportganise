2669feb2af2687ded0b067f8ec743ee7c829cbfb
Omar Alshanyour <80709119+HelloMeFriend@users.noreply.github.com>
2025-01-12 15:54:24 -0500

    Removing CrossOrigin

--- backend/src/main/java/com/sportganise/controllers/programsessions/ProgramController.java
+++ backend/src/main/java/com/sportganise/controllers/programsessions/ProgramController.java
@@ -13,7 +13,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -28,7 +27,6 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/programs")
-@CrossOrigin(origins = "*")
 public class ProgramController {
   private final ProgramService programService;
   private final AccountService accountService;
