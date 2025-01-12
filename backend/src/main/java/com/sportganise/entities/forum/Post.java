23f06bcc45295a37e6e2dc3c13c9d359c636d7da
Kaianinja <cl_manou@live.concordia.ca>
2025-01-12 16:58:22 -0500

    Adding search functionalities, api and tests

--- backend/src/main/java/com/sportganise/entities/forum/Post.java
+++ backend/src/main/java/com/sportganise/entities/forum/Post.java
@@ -1,10 +1,10 @@
 package com.sportganise.entities.forum;
 
-import jakarta.persistence.Id;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 import java.sql.Date;
