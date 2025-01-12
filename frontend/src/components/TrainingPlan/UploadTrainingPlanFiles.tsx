06aabbb186f096a7c26fe2a2b349fa6636ba7a66
Sofia Valiante <valiante.sofia@gmail.com>
2025-01-11 19:39:11 -0500

    feat(701): zod + react hook form setup

--- frontend/src/components/TrainingPlan/UploadTrainingPlanFiles.tsx
+++ frontend/src/components/TrainingPlan/UploadTrainingPlanFiles.tsx
@@ -1,7 +1,59 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import * as z from "zod";
+import { cn } from "@/lib/utils";
+
+import { Button } from "@/components/ui/Button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import {
+  FileInput,
+  FileUploader,
+  FileUploaderContent,
+  FileUploaderItem,
+} from "@/components/ui/file-upload";
+
+import { CloudUpload, Paperclip } from "lucide-react";
+
+const formSchema = z.object({
+  trainingPlans: z.array(
+    //array of files
+    z.custom<File>((file) => file instanceof File && file.size > 0, {
+      message: "Each file must be a valid file and not empty.",
+    })
+  ),
+});
+
 export default function UploadTrainingPlanFiles() {
-  return (
-    <div>
-      <p>this is UploadTrainingPlanFiles component on TrainingPlanPage</p>
-    </div>
-  );
+  const [files, setFiles] = useState<File[] | null>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const dropZoneConfig = {
+    maxFiles: 5,
+    maxSize: 1024 * 1024 * 4,
+    multiple: true,
+    accept: {
+      "application/doc": [".doc"],
+    },
+  };
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    try {
+    } catch (error) {
+      setError("An error occured! The files could not be uploaded.");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  return <div></div>;
 }
