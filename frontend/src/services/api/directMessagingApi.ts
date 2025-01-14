2705aab8b338690f0d6dd256820a002fbd33a02f
Sisahga <sisahga@gmail.com>
2025-01-14 12:20:37 -0500

    feat: implemented hook to and api fetch call to create a channel.
    Refs: #402

--- frontend/src/services/api/directMessagingApi.ts
+++ frontend/src/services/api/directMessagingApi.ts
@@ -1,14 +1,20 @@
-import { Channel } from "@/types/dmchannels.ts";
-import { MessageComponent } from "@/types/messaging.ts";
+import {Channel, CreateChannelDto} from "@/types/dmchannels.ts";
+import {MessageComponent} from "@/types/messaging.ts";
+import ResponseDto from "@/types/response.ts";
 
 const baseMappingUrl = import.meta.env.VITE_API_BASE_URL + "/api/messaging";
 
 const directMessagingApi = {
-  getDirectMessages: async (channelId: number | null) => {
-    const response = await fetch(
-      `${baseMappingUrl}/directmessage/get-messages/${channelId}`,
-    );
-    const data: MessageComponent[] = await response.json();
+  createChannel: async (channel: CreateChannelDto, creatorId: number): Promise<ResponseDto<CreateChannelDto>> => {
+    const response = await fetch(`${baseMappingUrl}/channel/create-channel/${creatorId}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(channel),
+    });
+    const data = await response.json();
+    console.log("Create Channel Response:", data);
     return data;
   },
   getChannels: async (accountId: number | null) => {
@@ -16,6 +22,13 @@ const directMessagingApi = {
       `${baseMappingUrl}/channel/get-channels/${accountId}`,
     );
     const data: Channel[] = await response.json();
+    return data;
+  },
+  getDirectMessages: async (channelId: number | null) => {
+    const response = await fetch(
+        `${baseMappingUrl}/directmessage/get-messages/${channelId}`,
+    );
+    const data: MessageComponent[] = await response.json();
     return data;
   },
   markChannelAsRead: async (
