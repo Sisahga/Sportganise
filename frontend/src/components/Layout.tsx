8bd59157847c2db5d6ecb37582ddbd2f0aea9ed2
Vithu <vithujanan0629@gmail.com>
2025-01-14 17:35:03 -0500

    The HeaderNav and FooterNav component should not be displayed on the login

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -7,10 +7,16 @@ import { Toaster } from "@/components/ui/toaster";
 const hideHeaderPaths = [
   "/pages/DirectMessagesDashboard",
   "/pages/DirectMessageChannelPage",
-]; // Header hidden on both
+  "/login",
+  "/signup",
+]; // Header hidden on all these pages
 
 // Paths where the footer should be hidden
-const hideFooterPaths = ["/pages/DirectMessageChannelPage"]; // Footer hidden only on /chat
+const hideFooterPaths = [
+  "/pages/DirectMessageChannelPage",
+  "/login",
+  "/signup",
+]; // Footer hidden on /chat, login and signup pages
 
 // Define paths where you don't want horizontal margins (like full-width pages)
 const fullWidthPaths = [
