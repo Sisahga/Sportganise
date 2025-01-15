24f5265c278e8e4c902ff96d8d089ad23a0d5941
Sofia Valiante <valiante.sofia@gmail.com>
2025-01-15 01:53:53 -0500

    feat(701): allowed more file types

--- frontend/src/components/TrainingPlan/UploadTrainingPlanForm.tsx
+++ frontend/src/components/TrainingPlan/UploadTrainingPlanForm.tsx
@@ -43,7 +43,13 @@ export default function UploadTrainingPlanFiles() {
     maxSize: 1024 * 1024 * 4,
     multiple: true,
     accept: {
-      "application/doc": [".doc"],
+      "application/msword": [".doc", ".dot"],
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.document":
+        [".docx"],
+      "application/vnd.openxmlformats-officedocument.wordprocessingml.template":
+        [".dotx"],
+      "application/vnd.ms-word.document.macroEnabled.12": [".docm"],
+      "application/vnd.ms-word.template.macroEnabled.12": [".dotm"],
     },
   };
 
