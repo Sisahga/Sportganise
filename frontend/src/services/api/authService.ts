ac6d726efebc966263f319959ba4a6d4d7981ce5
Kaianinja <cl_manou@live.concordia.ca>
2025-01-16 17:33:33 -0500

    Adding cookies at login

--- frontend/src/services/api/authService.ts
+++ frontend/src/services/api/authService.ts
@@ -1,4 +1,7 @@
+import log from "loglevel";
 import { LoginRequest, LoginResponse } from "@/types/auth";
+import { setCookies , isCookiesDto} from "@/services/cookiesService";
+
 
 const API_BASE_URL = "http://localhost:8080"; // API base URL
 
@@ -16,5 +19,13 @@ export const login = async (data: LoginRequest): Promise<LoginResponse> => {
     throw new Error(errorResponse || `HTTP error! status: ${response.status}`);
   }
 
-  return response.json(); // Parse response as JSON
+  const loginResponse: LoginResponse = await response.json();
+
+  if (loginResponse.data && isCookiesDto(loginResponse.data)) {
+    setCookies(loginResponse.data);
+  } else {
+    log.warn("No cookies data found in login response");
+  }
+
+  return loginResponse; // Parse response as JSON
 };
