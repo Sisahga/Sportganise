a3c7a72158de810def3c6b136c01e69016bdbf40
Sisahga <sisahga@gmail.com>
2025-01-16 20:35:40 -0500

    refactor: send message refactor.
    Refs: #198.

---
+++ frontend/src/hooks/useSendMessage.tsx
@@ -0,0 +1,16 @@
+import {SendMessageComponent} from "@/types/messaging.ts";
+import WebSocketService from "@/services/WebSocketService.ts";
+
+function useSendMessage() {
+  const sendDirectMessage
+      = (
+          messagePayload: SendMessageComponent,
+          webSocketServiceRef: WebSocketService | null
+        )=> {
+    webSocketServiceRef?.sendMessage(messagePayload);
+  }
+  return {
+    sendDirectMessage
+  }
+}
+export default useSendMessage;
