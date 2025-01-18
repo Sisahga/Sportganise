e1ccef44c9f2b807b5fd4a2a0f3f036f7f3852a4
Sisahga <sisahga@gmail.com>
2025-01-18 12:11:17 -0500

    feat: assigned enum types for roles of channel members (GROUP only)
    Refs: #475

--- backend/src/main/java/com/sportganise/entities/directmessaging/ChannelMemberRoleType.java
+++ backend/src/main/java/com/sportganise/entities/directmessaging/ChannelMemberRoleType.java
@@ -1,8 +1,6 @@
 package com.sportganise.entities.directmessaging;
 
-/**
- * Enum for the role of a ChannelMember.
- */
+/** Enum for the role of a ChannelMember. */
 public enum ChannelMemberRoleType {
   ADMIN,
   REGULAR
