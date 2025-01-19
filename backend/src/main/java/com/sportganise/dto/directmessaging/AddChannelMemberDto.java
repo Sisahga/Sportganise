3ee7babefb015728d4953a1067b15d31437b2857
Sisahga <sisahga@gmail.com>
2025-01-19 18:17:03 -0500

    feat: DTO to add a member to a channel.
    Refs: #489 #487

--- backend/src/main/java/com/sportganise/dto/directmessaging/AddChannelMemberDto.java
+++ backend/src/main/java/com/sportganise/dto/directmessaging/AddChannelMemberDto.java
@@ -1,13 +1,13 @@
 package com.sportganise.dto.directmessaging;
 
+import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.util.List;
-
+/** DTO for adding members to a Direct Message Channel. */
 @Getter
 @Setter
 @AllArgsConstructor
