dd59434cf752d111f8400f734dbd1507c848ab03
Sisahga <sisahga@gmail.com>
2025-01-19 02:28:40 -0500

    feat: controller route for adding members to a group channel.
    Refs: #489

--- backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageChannelMemberController.java
+++ backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageChannelMemberController.java
@@ -1,6 +1,7 @@
 package com.sportganise.controllers.directmessaging;
 
 import com.sportganise.dto.ResponseDto;
+import com.sportganise.dto.directmessaging.AddChannelMemberDto;
 import com.sportganise.dto.directmessaging.ChannelMembersDto;
 import com.sportganise.services.directmessaging.DirectMessageChannelMemberService;
 import jakarta.persistence.EntityNotFoundException;
@@ -9,12 +10,7 @@ import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /** Controller for Direct Message Channel Members. */
 @RestController
@@ -26,6 +22,36 @@ public class DirectMessageChannelMemberController {
   public DirectMessageChannelMemberController(
       DirectMessageChannelMemberService directMessageChannelMemberService) {
     this.directMessageChannelMemberService = directMessageChannelMemberService;
+  }
+
+  /**
+   * Add members to a channel.
+   *
+   * @param addChannelMemberDto The AddChannelMemberDto object.
+   * @return ResponseEntity with status 201 if successful, 500 if not.
+   */
+  @PostMapping("/add-members")
+  public ResponseEntity<ResponseDto<Null>> addMembersToChannel(
+          @RequestBody AddChannelMemberDto addChannelMemberDto) {
+    try {
+      this.directMessageChannelMemberService.saveMembers(
+              addChannelMemberDto.getMemberIds(),
+              addChannelMemberDto.getChannelId(),
+              addChannelMemberDto.getAdminId());
+      log.info("Successfully added members to channel: {}", addChannelMemberDto.getChannelId());
+      ResponseDto<Null> response =
+              new ResponseDto<>(HttpStatus.CREATED.value(), "Members added successfully", null);
+      return new ResponseEntity<>(response, HttpStatus.CREATED);
+    } catch (Exception e) {
+      log.error(
+              "Failed to add members to channel: {}. Error: {}", addChannelMemberDto.getChannelId(), e.getMessage());
+      ResponseDto<Null> response =
+              new ResponseDto<>(
+                      HttpStatus.INTERNAL_SERVER_ERROR.value(),
+                      "An unexpected error occurred while adding members to the channel",
+                      null);
+      return new ResponseEntity<>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
   }
 
   /**
