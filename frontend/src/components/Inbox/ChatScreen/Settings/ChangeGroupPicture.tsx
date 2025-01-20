05378718238d18e56693b68dc1910c9e825b8bab
Sisahga <sisahga@gmail.com>
2025-01-19 21:56:32 -0500

    refactor: moved to Inbox
    Refs: #480

--- frontend/src/components/Inbox/ChatScreen/Settings/ChangeGroupPicture.tsx
+++ frontend/src/components/Inbox/ChatScreen/Settings/ChangeGroupPicture.tsx
@@ -7,10 +7,10 @@ import {
   DialogHeader,
   DialogTitle,
   DialogFooter,
-} from "@/components/ui/dialog";
-import { Button } from "@/components/ui/Button";
-import { Label } from "@/components/ui/label";
-import { Input } from "@/components/ui/input";
+} from "@/components/ui/dialog.tsx";
+import { Button } from "@/components/ui/Button.tsx";
+import { Label } from "@/components/ui/label.tsx";
+import { Input } from "@/components/ui/input.tsx";
 
 interface ChangePictureDialogProps {
   isOpen: boolean;
