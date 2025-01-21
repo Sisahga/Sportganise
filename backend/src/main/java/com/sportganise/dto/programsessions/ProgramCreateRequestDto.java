207c574fe7482bc756b8351a08bd0d7373d025f0
Sisahga <sisahga@gmail.com>
2025-01-21 18:15:49 -0500

    style: format
    Refs: #12 #13

--- backend/src/main/java/com/sportganise/dto/programsessions/ProgramCreateRequestDto.java
+++ backend/src/main/java/com/sportganise/dto/programsessions/ProgramCreateRequestDto.java
@@ -5,9 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * API DTO for creating a new program.
-*/
+/** API DTO for creating a new program. */
 @Getter
 @Setter
 @NoArgsConstructor
