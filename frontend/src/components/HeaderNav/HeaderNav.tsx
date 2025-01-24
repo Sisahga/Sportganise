25e4603cc14bb0d388b87f37f1f7ada0ff316225
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2025-01-23 23:44:06 -0500

    fixed formatting
    fixing formatting

--- frontend/src/components/HeaderNav/HeaderNav.tsx
+++ frontend/src/components/HeaderNav/HeaderNav.tsx
@@ -57,8 +57,8 @@ export default function HeaderNav() {
                     Training Plan
                   </Link>
                   <Link
-                      to="/pages/WaitlistTrainingSessionPage"
-                      className="text-lg font-font font-medium bg-white text-primaryColour hover:text-secondaryColour inline-flex items-center justify-center"
+                    to="/pages/WaitlistTrainingSessionPage"
+                    className="text-lg font-font font-medium bg-white text-primaryColour hover:text-secondaryColour inline-flex items-center justify-center"
                   >
                     Waitlist
                   </Link>
