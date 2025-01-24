b5c82d55451e2835816573fb3969de3766aba04e
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2025-01-23 23:44:06 -0500

    fixed formatting
    fixing formatting

--- frontend/src/pages/WaitlistTrainingSessionPage.tsx
+++ frontend/src/pages/WaitlistTrainingSessionPage.tsx
@@ -3,13 +3,13 @@ import { HeaderNav } from "@/components/HeaderNav";
 import { FooterNav } from "@/components/FooterNav";
 
 export default function WaitlistTrainingSessionPage() {
-    return (
-        <div className="flex flex-col min-h-screen ">
-            <HeaderNav />
-            <div className="flex-grow">
-                <WaitlistTrainingSession/>
-            </div>
-            <FooterNav />
-        </div>
-    );
+  return (
+    <div className="flex flex-col min-h-screen ">
+      <HeaderNav />
+      <div className="flex-grow">
+        <WaitlistTrainingSession />
+      </div>
+      <FooterNav />
+    </div>
+  );
 }
