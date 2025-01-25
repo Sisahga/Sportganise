4474693b74304acf2abf741a02da7ee1e418cec2
Dannick B. Senss <dannickbsenss@gmail.com>
2025-01-24 19:47:46 -0500

    fix: reconfigure cors policy for deployed env

--- backend/src/main/java/com/sportganise/config/WebConfig.java
+++ backend/src/main/java/com/sportganise/config/WebConfig.java
@@ -12,7 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
   public void addCorsMappings(CorsRegistry registry) {
     registry
         .addMapping("/**")
-        .allowedOrigins("http://localhost:3000", "http://localhost:5173")
+        .allowedOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost")
         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
         .allowedHeaders("*")
         .allowCredentials(true)
