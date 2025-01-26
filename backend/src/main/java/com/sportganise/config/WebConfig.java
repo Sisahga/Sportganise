410045905d17629afc995e5ad3c978ac1a4b5139
Dannick B. Senss <dannickbsenss@gmail.com>
2025-01-26 14:03:56 -0500

    fix: allow frontend from vite in cors policy

--- backend/src/main/java/com/sportganise/config/WebConfig.java
+++ backend/src/main/java/com/sportganise/config/WebConfig.java
@@ -26,7 +26,8 @@ public class WebConfig implements WebMvcConfigurer {
   public FilterRegistrationBean<CorsFilter> corsFilter() {
     CorsConfiguration config = new CorsConfiguration();
     config.setAllowCredentials(true);
-    config.addAllowedOrigin("http://localhost:3000");
+    config.addAllowedOrigin("http://localhost:3000"); // Frontend sever
+    config.addAllowedOrigin("http://localhost:5173"); // Frontend running locally
     config.addAllowedOrigin("http://localhost");
     config.addAllowedOrigin("https://onibad.sportganise.com");
     config.addAllowedHeader("*");
