8f16137ed573fa80f6ed12abb78a440550a1de87
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2025-01-26 01:30:32 -0500

    fixed typo

--- frontend/src/components/PriceComparisonToolContent/PriceComparisonToolContent.tsx
+++ frontend/src/components/PriceComparisonToolContent/PriceComparisonToolContent.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Input } from "@/components/ui/input";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/components/ui/Button";
 import {
   Table,
   TableBody,
