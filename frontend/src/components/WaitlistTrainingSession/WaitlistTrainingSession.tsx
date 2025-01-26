c4ea3028f23ae750a018ee240c5df8b84fcbe295
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2025-01-26 16:20:59 -0500

    fixed bugs
    -confirm button shows to coach and admins
    -join queue shows to general
    -headearnav changed to show waitlist to general user
    -added cookies to identify user type
    -got rid of a file that cookies essentially replaced

--- frontend/src/components/WaitlistTrainingSession/WaitlistTrainingSession.tsx
+++ frontend/src/components/WaitlistTrainingSession/WaitlistTrainingSession.tsx
@@ -2,7 +2,7 @@ import WaitlistedTrainingSessionList from "./WaitlistedTrainingSessionList";
 
 export default function WaitlistTrainingSession() {
   return (
-    <div className="p-6">
+    <div className="p-6 mb-32 mt-32">
       <h1 className="text-2xl font-bold text-textColour">Waitlisted Players</h1>
       <p className="text-sm text-fadedPrimaryColour">
         The training sessions below are available for waitlisted members only.
