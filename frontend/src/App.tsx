c3c00750d56c4491cd24bfb53ce7ed35c4f0d92f
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2025-01-26 14:21:39 -0500

    fixed dependecies

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -37,56 +37,55 @@ function App() {
           path="/pages/CreateDmChannelPage"
           element={<CreateDmChannelPage />}
         ></Route>
-        <Route path="/" element={<Layout />}>
-          {" "}
-          {/*Place the routes to all your pages nested beneath this Route tag */}
-          <Route
-            path="/pages/ChangePasswordPage"
-            element={<ChangePasswordPage />}
-          ></Route>
-          <Route
-            path="/pages/CreateTrainingSessionPage"
-            element={<CreateTrainingSessionPage />}
-          ></Route>
-          <Route
-            path="/pages/DirectMessageChannelPage"
-            element={<DirectMessageChatPage />}
-          />
-          <Route
-            path="/pages/EditProfilePage"
-            element={<EditProfilePage />}
-          ></Route>
-          <Route path="/pages/ProfilePage" element={<ProfilePage />}></Route>
-          <Route
-            path="/pages/DirectMessagesDashboard"
-            element={<MessagingDashboard />}
-          />
-          <Route
-            path="/pages/PersonalInformationPage"
-            element={<PersonalInformationPage />}
-          ></Route>
-          <Route path="/pages/ProfilePage" element={<ProfilePage />}></Route>
-          <Route path="/login" element={<LogInPage />}></Route>
-          <Route path="/signup" element={<SignUpPage />}></Route>
-          <Route path="/verificationcode" element={<VerificationCodePage />} />
-          <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
-          <Route
-            path="/pages/ViewTrainingSessionPage"
-            element={<ViewTrainingSessionPage />}
-          ></Route>
-          <Route path="/pages/CalendarPage" element={<CalendarPage />}></Route>
-          <Route
-            path="/pages/ModifyTrainingSessionPage"
-            element={<ModifyTrainingSessionPage />}
-          ></Route>
-          <Route
-            path="/pages/PriceComparisonToolPage"
-            element={<PriceComparisonToolPage />}
-          ></Route>
-          <Route
-            path="/pages/BlockedUserListPage"
-            element={<BlockedUsersListPage />}
-          ></Route>
+        <Route path="/" element={<Layout />}></Route>
+        {/*Place the routes to all your pages nested beneath this Route tag */}
+        <Route
+          path="/pages/ChangePasswordPage"
+          element={<ChangePasswordPage />}
+        ></Route>
+        <Route
+          path="/pages/CreateTrainingSessionPage"
+          element={<CreateTrainingSessionPage />}
+        ></Route>
+        <Route
+          path="/pages/DirectMessageChannelPage"
+          element={<DirectMessageChatPage />}
+        />
+        <Route
+          path="/pages/EditProfilePage"
+          element={<EditProfilePage />}
+        ></Route>
+        <Route path="/pages/ProfilePage" element={<ProfilePage />}></Route>
+        <Route
+          path="/pages/DirectMessagesDashboard"
+          element={<MessagingDashboard />}
+        />
+        <Route
+          path="/pages/PersonalInformationPage"
+          element={<PersonalInformationPage />}
+        ></Route>
+        <Route path="/pages/ProfilePage" element={<ProfilePage />}></Route>
+        <Route path="/login" element={<LogInPage />}></Route>
+        <Route path="/signup" element={<SignUpPage />}></Route>
+        <Route path="/verificationcode" element={<VerificationCodePage />} />
+        <Route path="/forgotpassword" element={<ForgotPasswordPage />} />
+        <Route
+          path="/pages/ViewTrainingSessionPage"
+          element={<ViewTrainingSessionPage />}
+        ></Route>
+        <Route path="/pages/CalendarPage" element={<CalendarPage />}></Route>
+        <Route
+          path="/pages/ModifyTrainingSessionPage"
+          element={<ModifyTrainingSessionPage />}
+        ></Route>
+        <Route
+          path="/pages/PriceComparisonToolPage"
+          element={<PriceComparisonToolPage />}
+        ></Route>
+        <Route
+          path="/pages/BlockedUserListPage"
+          element={<BlockedUsersListPage />}
+        ></Route>
         <Route element={<PublicRoute />}>
           <Route path="/login" element={<LogInPage />}></Route>
           <Route path="/signup" element={<SignUpPage />}></Route>
