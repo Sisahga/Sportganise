3b46ff742f2f39903a0c49584709ce6469dc65f0
karyennevu <vuongkaryenne@gmail.com>
2025-02-04 01:42:46 -0500

    Moved logic to service layer

--- backend/src/main/java/com/sportganise/controllers/trainingplans/TrainingPlansController.java
+++ backend/src/main/java/com/sportganise/controllers/trainingplans/TrainingPlansController.java
@@ -2,10 +2,7 @@ package com.sportganise.controllers.trainingplans;
 
 import com.sportganise.dto.ResponseDto;
 import com.sportganise.dto.trainingplans.TrainingPlanResponseDto;
-import com.sportganise.entities.account.Account;
-import com.sportganise.services.account.AccountService;
 import com.sportganise.services.trainingplans.TrainingPlansService;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -24,13 +21,10 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 public class TrainingPlansController {
   private final TrainingPlansService trainingPlansService;
-  private final AccountService accountService;
 
   @Autowired
-  public TrainingPlansController(
-      TrainingPlansService trainingPlansService, AccountService accountService) {
+  public TrainingPlansController(TrainingPlansService trainingPlansService) {
     this.trainingPlansService = trainingPlansService;
-    this.accountService = accountService;
   }
 
   /**
@@ -43,49 +37,16 @@ public class TrainingPlansController {
   public ResponseEntity<ResponseDto<TrainingPlanResponseDto>> getTrainingPlans(
       @PathVariable Integer accountId) {
 
-    ResponseDto<TrainingPlanResponseDto> responseDto = new ResponseDto<>();
-
-    Optional<Account> userOptional = getAccount(accountId);
-
-    if (userOptional.isEmpty()) {
-      responseDto.setStatusCode(HttpStatus.NOT_FOUND.value());
-      responseDto.setMessage("User not found.");
-      return ResponseEntity.status(responseDto.getStatusCode()).body(responseDto);
-    }
-
-    Account user = userOptional.get();
-    log.debug("USER ID:", user.getAccountId());
-
-    if (!hasPermissions(user)) {
-      responseDto.setStatusCode(HttpStatus.FORBIDDEN.value());
-      responseDto.setMessage("Only Coaches and Admins can access this page.");
-      return ResponseEntity.status(responseDto.getStatusCode()).body(responseDto);
-    }
-
-    TrainingPlanResponseDto trainingPlanResponseDtos =
+    TrainingPlanResponseDto trainingPlanResponseDto =
         trainingPlansService.getTrainingPlans(accountId);
 
-    if (trainingPlanResponseDtos == null) {
-      responseDto.setStatusCode(HttpStatus.NOT_FOUND.value());
-      responseDto.setMessage("No training plans found.");
-      return ResponseEntity.status(responseDto.getStatusCode()).body(responseDto);
-    }
-
-    log.debug("TRAINING PLANS SHARED WITH ME COUNT: ", trainingPlanResponseDtos.getSharedWithMe());
-
+    ResponseDto<TrainingPlanResponseDto> responseDto = new ResponseDto<>();
     responseDto.setStatusCode(HttpStatus.OK.value());
     responseDto.setMessage("Training plans successfully fetched.");
-    responseDto.setData(trainingPlanResponseDtos);
+    responseDto.setData(trainingPlanResponseDto);
+
+    log.debug("TRAINING PLANS SHARED WITH ME COUNT: ", trainingPlanResponseDto.getSharedWithMe());
+
     return ResponseEntity.status(responseDto.getStatusCode()).body(responseDto);
-  }
-
-  /** Helper method to fetch and validate user account based on accountId. */
-  private Optional<Account> getAccount(Integer accountId) {
-    return accountService.getAccount(accountId);
-  }
-
-  /** Helper method to check user permissions. */
-  private boolean hasPermissions(Account user) {
-    return accountService.hasPermissions(user.getType());
   }
 }
--- backend/src/main/java/com/sportganise/services/trainingplans/TrainingPlansService.java
+++ backend/src/main/java/com/sportganise/services/trainingplans/TrainingPlansService.java
@@ -2,10 +2,15 @@ package com.sportganise.services.trainingplans;
 
 import com.sportganise.dto.trainingplans.TrainingPlanDto;
 import com.sportganise.dto.trainingplans.TrainingPlanResponseDto;
+import com.sportganise.entities.account.Account;
 import com.sportganise.entities.trainingplans.TrainingPlan;
+import com.sportganise.exceptions.ForbiddenException;
+import com.sportganise.exceptions.ResourceNotFoundException;
 import com.sportganise.repositories.trainingplans.TrainingPlansRepository;
+import com.sportganise.services.account.AccountService;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -15,14 +20,17 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class TrainingPlansService {
   private final TrainingPlansRepository trainingPlansRepository;
+  private final AccountService accountService;
 
   /**
    * Constructor for TrainingPlansService.
    *
    * @param trainingPlansRepository Repository for training plans.
    */
-  public TrainingPlansService(TrainingPlansRepository trainingPlansRepository) {
+  public TrainingPlansService(
+      TrainingPlansRepository trainingPlansRepository, AccountService accountService) {
     this.trainingPlansRepository = trainingPlansRepository;
+    this.accountService = accountService;
   }
 
   /**
@@ -31,7 +39,16 @@ public class TrainingPlansService {
    * @return List of Training Plan Dtos.
    */
   public TrainingPlanResponseDto getTrainingPlans(Integer userId) {
+    Account user = getUser(userId);
+
+    if (!hasPermissions(user)) {
+      throw new ForbiddenException("Only Coaches and Admins can access this page.");
+    }
+
     List<TrainingPlan> trainingPlans = trainingPlansRepository.findTrainingPlans();
+    if (trainingPlans.isEmpty()) {
+      throw new ResourceNotFoundException("No training plans found.");
+    }
     log.debug("PROGRAMS COUNT: ", trainingPlans.size());
 
     List<TrainingPlanDto> trainingPlanDtos = new ArrayList<>();
@@ -64,5 +81,43 @@ public class TrainingPlansService {
         new TrainingPlanResponseDto(myPlans, sharedWithMe);
 
     return trainingPlanResponseDto;
+  }
+
+  /**
+   * Helper method to get user Account object.
+   *
+   * @param accountId Id of user account.
+   * @return Account object.
+   */
+  private Account getUser(Integer accountId) {
+    Optional<Account> userOptional = getAccountById(accountId);
+
+    if (userOptional.isEmpty()) {
+      throw new ResourceNotFoundException("User not found.");
+    }
+
+    Account user = userOptional.get();
+    log.debug("USER ID:", user.getAccountId());
+    return user;
+  }
+
+  /**
+   * Helper method to fetch and validate user account based on accountId.
+   *
+   * @param accountId Id of Account.
+   * @return return an Optional of Account object.
+   */
+  private Optional<Account> getAccountById(Integer accountId) {
+    return accountService.getAccount(accountId);
+  }
+
+  /**
+   * Helper method to check user permissions.
+   *
+   * @param user Account object.
+   * @return Whether user has permission or not.
+   */
+  private boolean hasPermissions(Account user) {
+    return accountService.hasPermissions(user.getType());
   }
 }
