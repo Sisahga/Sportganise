458b51c1b8ec69e1d0dc37ff0d5f080cf6f3ecea
j3rrimmy <98069409+j3rrimmy@users.noreply.github.com>
2025-02-04 19:22:03 -0500

    fixing functionalities and reformating

--- frontend/src/components/WaitlistTrainingSession/WaitlistTrainingSession.tsx
+++ frontend/src/components/WaitlistTrainingSession/WaitlistTrainingSession.tsx
@@ -1,13 +1,21 @@
 import WaitlistedTrainingSessionList from "./WaitlistedTrainingSessionList";
+import { Program } from "@/types/trainingSessionDetails.ts";
 
-export default function WaitlistTrainingSession() {
+interface WaitlistTrainingSessionProps {
+  onSelectTraining: (program: Program) => void;
+}
+
+export default function WaitlistTrainingSession({
+  onSelectTraining,
+}: WaitlistTrainingSessionProps) {
   return (
     <div className="p-6 mb-32 mt-32">
       <h1 className="text-2xl font-bold text-textColour">Waitlisted Players</h1>
       <p className="text-sm text-fadedPrimaryColour">
         The training sessions below are available for waitlisted members only.
       </p>
-      <WaitlistedTrainingSessionList />
+
+      <WaitlistedTrainingSessionList onSelectTraining={onSelectTraining} />
     </div>
   );
 }
