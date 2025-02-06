dbba7677e6dbae9027ff8d515288a3e39543e9f1
Kaianinja <cl_manou@live.concordia.ca>
2025-02-05 22:13:53 -0500

    formatting postDTO

--- backend/src/main/java/com/sportganise/dto/forum/CreateFeedbackDto.java
+++ backend/src/main/java/com/sportganise/dto/forum/CreateFeedbackDto.java
@@ -1,15 +1,20 @@
 package com.sportganise.dto.forum;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+/** Data Transfer Object for creating a feedback. */
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class CreateFeedbackDto {
-    private Integer postId;
-    private Integer accountId;
-    private String title;
-    private String content;
+  private Integer postId;
+  private Integer accountId;
+  private String title;
+  private String content;
 }
--- backend/src/main/java/com/sportganise/dto/forum/ViewPostDto.java
+++ backend/src/main/java/com/sportganise/dto/forum/ViewPostDto.java
@@ -1,26 +1,28 @@
 package com.sportganise.dto.forum;
 
-import com.sportganise.entities.forum.Feedback;
 import com.sportganise.entities.forum.PostType;
 import jakarta.validation.constraints.NotNull;
-import lombok.*;
-
 import java.time.ZonedDateTime;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+/** Data Transfer Object for ViewPost. */
 @Builder
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ViewPostDto {
-    @NotNull
-    private Integer postId;
-    @NotNull private String title;
-    @NotNull private String description;
-    private PostType type;
-    private ZonedDateTime occurrenceDate;
-    @NotNull private ZonedDateTime creationDate;
-    @NotNull private Long likeCount;
-    @NotNull private List<FeedbackDto> feedbackCount;
+  @NotNull private Integer postId;
+  @NotNull private String title;
+  @NotNull private String description;
+  private PostType type;
+  private ZonedDateTime occurrenceDate;
+  @NotNull private ZonedDateTime creationDate;
+  @NotNull private Long likeCount;
+  @NotNull private List<FeedbackDto> feedbackCount;
 }
