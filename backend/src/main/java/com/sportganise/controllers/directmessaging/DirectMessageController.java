bc225d6cf93876acf1ef3cc9417e2f3f1e9b544d
Sisahga <sisahga@gmail.com>
2025-02-05 21:54:35 -0500

    feat: updated backend to handle attachment uploads for a direct message. Separated message sending and attachment sending as two separate services and endpoints.
    Refs: #485, #196

--- backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageController.java
+++ backend/src/main/java/com/sportganise/controllers/directmessaging/DirectMessageController.java
@@ -1,24 +1,24 @@
 package com.sportganise.controllers.directmessaging;
 
+import com.sportganise.dto.ResponseDto;
 import com.sportganise.dto.directmessaging.DirectMessageDto;
 import com.sportganise.dto.directmessaging.SendDirectMessageRequestDto;
-import com.sportganise.entities.account.Account;
-import com.sportganise.repositories.AccountRepository;
 import com.sportganise.services.directmessaging.DirectMessageService;
 import java.util.List;
-import java.util.Objects;
-import java.util.Optional;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /** Controller for direct messaging. */
 @RestController
@@ -27,12 +27,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class DirectMessageController {
 
   private final DirectMessageService directMessageService;
-  private final AccountRepository accountRepository;
+  private final SimpMessagingTemplate simpMessagingTemplate;
 
   public DirectMessageController(
-      DirectMessageService directMessageService, AccountRepository accountRepository) {
+          DirectMessageService directMessageService, SimpMessagingTemplate simpMessagingTemplate) {
     this.directMessageService = directMessageService;
-    this.accountRepository = accountRepository;
+    this.simpMessagingTemplate = simpMessagingTemplate;
   }
 
   /**
@@ -64,30 +64,26 @@ public class DirectMessageController {
     }
   }
 
-  /**
-   * Notifies a channel that a member was added in the channel. (NOT USED)
-   *
-   * @param messageDto Direct message request DTO for adding a member.
-   * @param headerAccessor SimpMessageHeaderAccessor for the web socket session.
-   * @return Response DTO for adding a member.
-   */
-  // TODO (In Progress, next sprint): Create association between new member and channel in database.
-  @MessageMapping("/chat.add-member")
-  @SendTo("/directmessage/public")
-  public DirectMessageDto addMember(
-      @Payload SendDirectMessageRequestDto messageDto, SimpMessageHeaderAccessor headerAccessor) {
+  @PostMapping("/upload-attachments")
+  public ResponseEntity<ResponseDto<DirectMessageDto>> uploadAttachments(
+          @RequestParam("attachments") List<MultipartFile> attachments,
+          @RequestParam("messageId") int messageId,
+          @RequestParam("senderId") int senderId,
+          @RequestParam("senderFirstName") String senderFirstName,
+          @RequestParam("senderAvatarUrl") String senderAvatarUrl) {
     try {
-      DirectMessageDto responseDto = directMessageService.addMember(messageDto);
-      // Expect content to be id of new user when adding a new user to a message channel.
-      int newUserId = Integer.parseInt(messageDto.getMessageContent());
-      Optional<Account> newMember = accountRepository.findById(newUserId);
-      // Add username in web socket session
-      newMember.ifPresent(
-          account ->
-              Objects.requireNonNull(headerAccessor.getSessionAttributes())
-                  .put("username", account.getFirstName()));
-      return responseDto;
+      log.info("Controller received attachment upload request.");
+      DirectMessageDto updatedMessage = directMessageService
+              .uploadAttachments(messageId, attachments, senderId, senderFirstName, senderAvatarUrl);
+      simpMessagingTemplate.convertAndSend("/directmessage/public", updatedMessage);
+      ResponseDto<DirectMessageDto> response = ResponseDto.<DirectMessageDto>builder()
+              .statusCode(HttpStatus.OK.value())
+              .message("Attachments uploaded successfully.")
+              .data(updatedMessage)
+              .build();
+      return ResponseEntity.ok(response);
     } catch (Exception e) {
+      log.error("Error uploading attachments", e);
       return null;
     }
   }
