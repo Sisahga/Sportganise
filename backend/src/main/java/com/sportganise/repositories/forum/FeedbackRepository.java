6435494ed76417e96dadbde7ab21f1ca437ec74b
Kaianinja <cl_manou@live.concordia.ca>
2025-02-05 22:32:12 -0500

    Adding Repository Methods

--- backend/src/main/java/com/sportganise/repositories/forum/FeedbackRepository.java
+++ backend/src/main/java/com/sportganise/repositories/forum/FeedbackRepository.java
@@ -1,6 +1,7 @@
 package com.sportganise.repositories.forum;
 
 import com.sportganise.entities.forum.Feedback;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -15,4 +16,34 @@ public interface FeedbackRepository extends JpaRepository<Feedback, Integer> {
    * @return Number of feedbacks.
    */
   long countByPostId(Integer postId);
+
+  /**
+   * Fetches feedback by feedback id.
+   *
+   * @param feedbackId Feedback id.
+   * @return Feedback.
+   */
+  <Optional> Feedback findByFeedbackId(Integer feedbackId);
+
+  /**
+   * Fetches feedbacks by post id.
+   *
+   * @param postId Post id.
+   * @return List of feedbacks.
+   */
+  <Optional> List<Feedback> findFeedbacksByPostId(Integer postId);
+
+  /**
+   * Deletes feedback by post id.
+   *
+   * @param postId Post id.
+   */
+  void deleteByPostId(Integer postId);
+
+  /**
+   * Deletes feedback by feedback id.
+   *
+   * @param feedbackId Feedback id.
+   */
+  void deleteByFeedbackId(Integer feedbackId);
 }
