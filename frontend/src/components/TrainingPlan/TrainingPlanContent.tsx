c3ab40bf3365e04a50ee73ac01edf308b969f6e3
Sofia Valiante <valiante.sofia@gmail.com>
2025-02-08 22:11:28 -0500

    feat(702): hook, types, utils, service
    - hook useTrainingPlans()
    - Api service to fetch training plans
    - interface types for TrainingPlan and TrainingPlanDto
    - util helper function getDate()
    - util helper function getFileName()
    - logging
    - improved readability and followed style format
    - updated column fields and prep data
    - return myTrainingPlans and shareTrainingPlans separately
    - loading and error states
    - props data for shareTrainingPlans and myTrainingPlans
    - connect be to fe
    - added getFileName to TrainingSessionsList

--- frontend/src/components/TrainingPlan/TrainingPlanContent.tsx
+++ frontend/src/components/TrainingPlan/TrainingPlanContent.tsx
@@ -1,8 +1,63 @@
+// React
+import { useEffect } from "react";
+import { useNavigate } from "react-router";
+// Components
 import TrainingPlanTable from "./TrainingPlanTable";
 import { AddTrainingPlanButton } from "@/components/TrainingPlan";
+// UI Components
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+// Icons
+import { Loader2 } from "lucide-react";
+// Hooks
+import useTrainingPlans from "@/hooks/useTrainingPlans";
+// Services
+import { getCookies, getAccountIdCookie } from "@/services/cookiesService";
+// Logs
+import log from "loglevel";
 
 export default function TrainingPlanContent() {
+  const navigate = useNavigate();
+
+  // Get AccountId From Cookie
+  const cookies = getCookies();
+  const accountId = cookies ? getAccountIdCookie(cookies) : null;
+  useEffect(() => {
+    if (!accountId) {
+      log.debug("TrainingPlanContent -> No accountId found");
+    }
+    log.info(`TrainingPlanContent -> accountId is ${accountId}`);
+  }, [accountId]);
+
+  // Reroute User By Account Type
+  useEffect(() => {
+    if (!cookies || cookies.type === "GENERAL" || cookies.type === "PLAYER") {
+      navigate("/");
+    }
+    log.debug(`TrainingPlanContent -> accountType is ${cookies?.type}`);
+  }, [navigate, cookies]);
+
+  // Block Page for Null AccountId
+  if (!accountId) {
+    return (
+      <div className="flex flex-col items-center justify-center min-h-screen">
+        <div className="font-semibold text-center text-red text-xl md:text-2xl lg:text-3xl mb-3">
+          An error occurred!
+        </div>
+        <div className="text-center">Could not find your Account!</div>
+        <div className="text-center">Please log out and log back in ✔</div>
+      </div>
+    );
+  }
+
+  // Fetch Training Plans Created By and Shared With Current User
+  const { myTrainingPlans, sharedTrainingPlans, loading, error } =
+    useTrainingPlans(accountId); // myTrainingPlans, sharedTrainingPlans = [] can be passed to <TrainingPlanTable />
+  log.info("TrainingPlanContent -> myTrainingPlans are", myTrainingPlans);
+  log.info(
+    "TrainingPlanContent -> sharedTrainingPlans are",
+    sharedTrainingPlans
+  );
+
   return (
     <div className="mt-5 mb-32 ">
       <p className="text-3xl font-semibold text-center text-secondaryColour">
@@ -17,10 +72,32 @@ export default function TrainingPlanContent() {
             </TabsList>
           </div>
           <TabsContent value="mine">
-            <TrainingPlanTable />
+            {loading ? (
+              <Loader2
+                className="animate-spin m-5 justify-self-center"
+                color="rgb(107 114 128 / var(--tw-text-opacity, 1))"
+              />
+            ) : error ? (
+              <p className="m-5 text-red text-center">
+                Error! Could not get training plans.
+              </p>
+            ) : (
+              <TrainingPlanTable data={myTrainingPlans} />
+            )}
           </TabsContent>
           <TabsContent value="shared">
-            Training Plans shared with me.
+            {loading ? (
+              <Loader2
+                className="animate-spin m-5 justify-self-center"
+                color="rgb(107 114 128 / var(--tw-text-opacity, 1))"
+              />
+            ) : error ? (
+              <p className="m-5 text-red text-center">
+                Error! Could not get training plans.
+              </p>
+            ) : (
+              <TrainingPlanTable data={sharedTrainingPlans} />
+            )}
           </TabsContent>
         </Tabs>
       </div>
---
+++ frontend/src/hooks/useTrainingPlans.ts
@@ -0,0 +1,65 @@
+import { useState, useEffect } from "react";
+import ResponseDto from "@/types/response";
+import { TrainingPlan, TrainingPlansDto } from "@/types/trainingplans";
+import trainingPlanApi from "@/services/api/trainingPlanApi";
+import log from "loglevel";
+
+/**
+ * Fetches all training plans created by and shared with the current user.
+ *
+ * @param accountId
+ * @returns myTrainingPlans[], sharedTrainingPlans[], loading, error
+ */
+function useTrainingPlans(accountId: number | null | undefined) {
+  const [myTrainingPlans, setMyTrainingPlans] = useState<TrainingPlan[]>([]); // Training plans created by current user
+  const [sharedTrainingPlans, setSharedTrainingPlans] = useState<
+    TrainingPlan[]
+  >([]); // Training plans shared with current user
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const fetchTrainingPlans = async () => {
+    try {
+      // Call API
+      const response: ResponseDto<TrainingPlansDto> =
+        await trainingPlanApi.fetchTrainingPlans(accountId); // statusCode, message, data
+      // Check Status Code
+      if (response.statusCode !== 200) {
+        throw new Error(
+          `Could not fetch training plan(s). Status code ${response.statusCode}`
+        );
+      }
+      // If Response.ok, Initialize States
+      setMyTrainingPlans(response.data?.myPlans ?? []); // Return [] if response.data is null | undefined
+      setSharedTrainingPlans(response.data?.sharedWithMe ?? []);
+      // Log Info
+      log.info(
+        "useTrainingPlans.fetchTrainingPlans -> Fetched my training plans:",
+        myTrainingPlans
+      );
+      log.info(
+        "useTrainingPlans.fetchTrainingPlans -> Fetched training plans shared with me:",
+        sharedTrainingPlans
+      );
+    } catch (err) {
+      // Handle Error
+      setError("Error fetching training plan(s)!");
+      console.error(err);
+      log.error(err);
+    }
+  };
+  // Call fetchTrainingPlans() and Update States Safely
+  useEffect(() => {
+    fetchTrainingPlans().then(() => {
+      setLoading(false);
+    });
+  }, []);
+  return {
+    myTrainingPlans,
+    sharedTrainingPlans,
+    loading,
+    error,
+  };
+}
+
+export default useTrainingPlans;
