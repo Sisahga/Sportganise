0f7e3d19528b7e3664a9ca91bf19f8c0977ef0c6
Omar Alshanyour <80709119+HelloMeFriend@users.noreply.github.com>
2025-02-10 23:50:06 -0500

    refactor: format code

--- frontend/src/components/ViewTrainingSessions/RegisteredPlayer.tsx
+++ frontend/src/components/ViewTrainingSessions/RegisteredPlayer.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState } from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
 import { User2Icon } from "lucide-react";
 import AttendeeBadgeType from "./BadgeTypes/AttendeeBadgeType";
@@ -25,33 +25,39 @@ const RegisteredPlayer: React.FC<RegisteredPlayerProps> = ({
   return (
     <div>
       <div onClick={() => setIsModalOpen(true)} className="cursor-pointer">
-      <div className="flex my-2">
-        <div className="mr-4 self-center">
-          <Avatar>
-            <AvatarImage src={accountDetails?.pictureUrl} />
-            <AvatarFallback>
-              <User2Icon color="#a1a1aa" />
-            </AvatarFallback>
-          </Avatar>
+        <div className="flex my-2">
+          <div className="mr-4 self-center">
+            <Avatar>
+              <AvatarImage src={accountDetails?.pictureUrl} />
+              <AvatarFallback>
+                <User2Icon color="#a1a1aa" />
+              </AvatarFallback>
+            </Avatar>
+          </div>
+          <div>
+            {loading ? (
+              <p className="text-cyan-300 text-sm font-normal mb-1">
+                Loading...
+              </p>
+            ) : error ? (
+              <p className="text-red text-sm font-normal mb-1">
+                Failed to load account details
+              </p>
+            ) : (
+              <h4 className="text-sm font-normal mb-1">
+                {accountDetails?.firstName} {accountDetails?.lastName}
+              </h4>
+            )}
+            <AttendeeBadgeType accountType={accountDetails?.type} />
+          </div>
         </div>
-        <div>
-          {loading ? (
-            <p className="text-cyan-300 text-sm font-normal mb-1">Loading...</p>
-          ) : error ? (
-            <p className="text-red text-sm font-normal mb-1">
-              Failed to load account details
-            </p>
-          ) : (
-            <h4 className="text-sm font-normal mb-1">
-              {accountDetails?.firstName} {accountDetails?.lastName}
-            </h4>
-          )}
-          <AttendeeBadgeType accountType={accountDetails?.type} />
-        </div>
+        <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
       </div>
-      <hr className="h-px bg-gray-200 border-0 dark:bg-gray-700" />
-      </div>
-      <ParticipantPopUp accountId={accountId} isOpen={isModalOpen} onClose={() => setIsModalOpen(false)} />
+      <ParticipantPopUp
+        accountId={accountId}
+        isOpen={isModalOpen}
+        onClose={() => setIsModalOpen(false)}
+      />
     </div>
   );
 };
