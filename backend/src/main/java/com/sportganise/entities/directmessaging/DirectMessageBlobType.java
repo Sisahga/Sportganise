8224a39fa4de3baffe20ec2e076462250e08758a
Sisahga <sisahga@gmail.com>
2025-02-16 15:03:36 -0500

    feat: added file type as field in message_blob and made a composite key out of message_id and blob_url. File type was added to be served in frontend for ui purposes when showing images vs. files in dms.
    Refs: #196 #485

---
+++ backend/src/main/java/com/sportganise/entities/directmessaging/DirectMessageBlobType.java
@@ -0,0 +1,10 @@
+package com.sportganise.entities.directmessaging;
+
+/**
+ * Enum for the type of blob that is attached to a direct message.
+ */
+public enum DirectMessageBlobType {
+  IMAGE,
+  VIDEO,
+  FILE
+}
