805b6b9d335e35f690608eb4a64d5bc2f0e4ebab
Sisahga <sisahga@gmail.com>
2025-02-16 15:16:43 -0500

    style: formatting be.
    Refs: #196 #485

--- backend/src/main/java/com/sportganise/entities/directmessaging/DirectMessageBlobCompositeKey.java
+++ backend/src/main/java/com/sportganise/entities/directmessaging/DirectMessageBlobCompositeKey.java
@@ -9,9 +9,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-/**
- * DirectMessageBlobCompositeKey.
- */
+/** DirectMessageBlobCompositeKey. */
 @Getter
 @Setter
 @NoArgsConstructor
