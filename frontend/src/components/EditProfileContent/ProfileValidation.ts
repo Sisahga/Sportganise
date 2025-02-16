6956ec3bb220aa886a9cfe349c0a0fb415e688c6
nusrath-z <nusrath.zaman.nz@gmail.com>
2025-02-16 18:29:01 -0500

    refactor(202): Changed fields from required to optional

--- frontend/src/components/EditProfileContent/ProfileValidation.ts
+++ frontend/src/components/EditProfileContent/ProfileValidation.ts
@@ -4,35 +4,37 @@ import { z } from "zod";
 export const profileSchema = z.object({
   firstName: z
     .string()
-    .min(1, "First name is required")
-    .regex(/^[a-zA-Z]+$/, "Only letters are allowed"),
+    .regex(/^[a-zA-Z]+$/, "Only letters are allowed")
+    .optional()
+    .or(z.literal("")),
   lastName: z
     .string()
-    .min(1, "Last name is required")
-    .regex(/^[a-zA-Z]+$/, "Only letters are allowed"),
-  email: z.string().min(1, "Email is required").email("Invalid email address"),
+    .regex(/^[a-zA-Z]+$/, "Only letters are allowed")
+    .optional()
+    .or(z.literal("")),
+  email: z.string().email("Invalid email address").optional().or(z.literal("")),
   phone: z
     .string()
     .min(10, "Phone number must be 10 digits")
     .regex(
       /^\d{3}-\d{3}-\d{4}$/,
       "Phone number must be in the format 222-222-2222",
-    ),
-  address: z.string().min(1, "Address is required"),
-  city: z.string().min(1, "City is required"),
-  province: z.string().min(1, "Province is required"),
+    )
+    .optional()
+    .or(z.literal("")),
+  address: z.string().optional().or(z.literal("")),
+  city: z.string().optional().or(z.literal("")),
+  province: z.string().optional().or(z.literal("")),
   postalCode: z
     .string()
-    .min(1, "Postal code is required")
-    .regex(
-      /^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/,
-      "Invalid postal code: X1X 1X1.",
-    ),
-  country: z.string().min(1, "Country is required"),
+    .regex(/^[A-Za-z]\d[A-Za-z] \d[A-Za-z]\d$/, "Invalid postal code: X1X 1X1.")
+    .optional()
+    .or(z.literal("")),
+  country: z.string().optional().or(z.literal("")),
 });
 
 export type ProfileFormValues = z.infer<typeof profileSchema>;
 
-//Image Validation
+// Image Validation
 export const allowedImageTypes = ["image/jpeg", "image/png", "image/jpg"];
 export const maxFileSizeInBytes = 2 * 1024 * 1024;
