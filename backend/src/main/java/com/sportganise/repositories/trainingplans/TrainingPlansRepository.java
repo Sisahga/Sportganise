54804b28a7824321f94a7189a7fca7d5df2b618c
karyennevu <vuongkaryenne@gmail.com>
2025-02-17 18:51:08 -0500

    Added repository query to find a single training plan

--- backend/src/main/java/com/sportganise/repositories/trainingplans/TrainingPlansRepository.java
+++ backend/src/main/java/com/sportganise/repositories/trainingplans/TrainingPlansRepository.java
@@ -4,6 +4,7 @@ import com.sportganise.entities.trainingplans.TrainingPlan;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 /** Repository for Training Plans. */
@@ -16,5 +17,10 @@ public interface TrainingPlansRepository extends JpaRepository<TrainingPlan, Int
             """)
   List<TrainingPlan> findTrainingPlans();
 
-  
+  @Query("""
+            SELECT tp
+            FROM TrainingPlan tp
+            WHERE tp.planId = :planId
+            """)
+  TrainingPlan findTrainingPlan(@Param("planId") Integer planId);
 }
