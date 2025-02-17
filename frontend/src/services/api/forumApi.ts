56cf828c73ca7fc457b755445d07dd47f2e7b501
nusrath-z <nusrath.zaman.nz@gmail.com>
2025-02-17 04:15:57 -0500

    refactor(501,502,503): updated frontend API endpoints and response to align with backend

--- frontend/src/services/api/forumApi.ts
+++ frontend/src/services/api/forumApi.ts
@@ -1,47 +1,61 @@
 import { getBearerToken } from "@/services/apiHelper";
 import { PostDto, PostResponse } from "@/types/forum";
 import { getCookies } from "../cookiesService";
+import log from "loglevel";
 
 const baseMappingUrl = import.meta.env.VITE_API_BASE_URL + "/api/forum";
 
-//FETCH POSTS API
+/**
+ * Fetches posts based on various query parameters.
+ * - searchTerm: (Optional) Term to search in posts.
+ * - occurrenceDate: (Optional) Date of occurrence to filter posts.
+ * - type: (Optional) Type of posts to filter.
+ * - limit: (Optional) Limit of posts to fetch.
+ * - page: (Optional) Page number for pagination.
+ * - sortBy: (Optional) Sort posts by a specific field.
+ * - sortDir: (Optional) Sort direction (ascending or descending).
+ * @returns Promise<PostDto[]> List of posts.
+ */
 const fetchPosts = async (
   searchTerm?: string,
   occurrenceDate?: string,
   type?: string,
-  selectedLabel?: string,
   limit?: number,
   page?: number,
   sortBy?: string,
-  sortDir?: string
+  sortDir?: string,
 ): Promise<PostDto[]> => {
-  const orgId = 1;  //change when org id setup properly
+  const orgId = 1; //change when org id setup properly
   const user = getCookies();
-  let url = `${baseMappingUrl}/posts/search?orgId=${orgId}&accountId=${user.accountId}`;
+  let url = `${baseMappingUrl}/posts/search/${orgId}/${user.accountId}?`;
 
+  const queryParams = [];
 
-  if (searchTerm) url += `&searchTerm=${searchTerm}`;
-  if (occurrenceDate) url += `&occurrenceDate=${occurrenceDate}`;
-  if (type) url += `&type=${type}`;
-  if (selectedLabel) url += `&selectedLabel=${selectedLabel}`;
-  if (limit) url += `&limit=${limit}`;
-  if (page) url += `&page=${page - 1}`;
-  if (sortBy) url += `&sortBy=${sortBy}`;
-  if (sortDir) url += `&sortDir=${sortDir}`;
+  if (searchTerm) queryParams.push(`searchTerm=${searchTerm}`);
+  if (occurrenceDate) queryParams.push(`occurrenceDate=${occurrenceDate}`);
+  if (type) queryParams.push(`type=${type}`);
+  if (limit) queryParams.push(`limit=${limit}`);
+  if (page) queryParams.push(`page=${page - 1}`);
+  if (sortBy) queryParams.push(`sortBy=${sortBy}`);
+  if (sortDir) queryParams.push(`sortDir=${sortDir}`);
 
-  console.log("Fetching posts from URL:", url);
+  if (queryParams.length > 0) {
+    url += queryParams.join("&");
+  }
+
+  log.info("Fetching posts from URL:", url);
   const response = await fetch(url, {
-    method: 'GET',
+    method: "GET",
     headers: {
       Authorization: `Bearer ${getBearerToken()}`,
-      'Content-Type': 'application/json',
+      "Content-Type": "application/json",
     },
   });
 
   const data: PostResponse = await response.json();
 
   if (data.statusCode === 200) {
-    return data.data; 
+    return data.data;
   } else {
     throw new Error("Failed to fetch posts");
   }
--- frontend/src/services/api/postAPI.ts
+++ frontend/src/services/api/postAPI.ts
@@ -1,141 +1,179 @@
 import { getBearerToken } from "@/services/apiHelper";
 import { Post, ApiResponse } from "@/types/postdetail";
+import { getCookies } from "../cookiesService";
+import log from "loglevel";
 
+const BASE_API_URL = import.meta.env.VITE_API_BASE_URL + "/api/forum/posts";
 
-const baseMappingUrl = import.meta.env.VITE_API_BASE_URL + "/api/forum/posts";
+/**
+ * Post APIs
+ * - getPost(postId, accountId) -> fetches post details by ID
+ * - likePost(postId, accountId) -> likes a post
+ * - unlikePost(postId, accountId) -> unlikes a post
+ * - addFeedback(postId, accountId, content) -> adds feedback to a post
+ * - deleteFeedback(postId, feedbackId) -> deletes feedback from a post
+ */
 const postApi = {
-    async getPost(postId: number): Promise<ApiResponse<Post>> {
-        console.log("Fetching post API, Post ID:", postId);
-        console.log(baseMappingUrl)
+  // Get Post Details
+  getPost: async (postId: number): Promise<ApiResponse<Post>> => {
+    const user = getCookies();
+    log.debug(
+      "Fetching post API, Post ID:",
+      postId,
+      user.accountId,
+      BASE_API_URL,
+    );
 
-        const url = `${baseMappingUrl}/${postId}`;
-        console.log("Request URL:", url);
+    const url = `${BASE_API_URL}/${postId}/${user.accountId}`;
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${getBearerToken()}`,
+        "Content-Type": "application/json",
+      },
+    });
 
-        const response = await fetch(url, {
-            method: "GET",
-            headers: {
-                "Authorization": `Bearer ${getBearerToken()}`,
-                "Content-Type": "application/json"
-            }
-        });
-
-        console.log("Response status:", response.status);
-        if (!response.ok) {
-            const error = await response.json();
-            console.log("Error fetching post:", error);
-            throw new Error(error?.message || `Error fetching post with ID ${postId}`);
-        }
-
-        const data = await response.json();
-        console.log("Fetched post data:", data);
-        return data;
-    },
-
-    async likePost(postId: number, accountId: number): Promise<ApiResponse<null>> {
-        console.log("Liking post API, Post ID:", postId, "Account ID:", accountId);
-
-        const url = `${baseMappingUrl}/${postId}/like`;
-        console.log("Request URL:", url);
-
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${getBearerToken()}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ accountId })
-        });
-
-        if (response.status === 500) {
-            const error = await response.json();
-            console.log("Error Code:", response.status)
-            throw new Error(error?.message || `Error liking post with ID ${postId}`);
-        }
-
-        const data = await response.json();
-        console.log("Post liked successfully, response data:", data);
-        return data;
-    },
-
-    async unlikePost(postId: number, accountId: number): Promise<void> {
-        console.log("Unliking post API, Post ID:", postId, "Account ID:", accountId);
-
-        const url = `${baseMappingUrl}/${postId}/unlike/${accountId}`;
-        console.log("Request URL:", url);
-
-        const response = await fetch(url, {
-            method: "DELETE",
-            headers: {
-                "Authorization": `Bearer ${getBearerToken()}`
-            }
-        });
-
-        console.log("Response status:", response.status);
-        if (!response.ok) {
-            const error = await response.json();
-            console.log("Error unliking post:", error);
-            throw new Error(error?.message || `Error unliking post with ID ${postId}`);
-        }
-
-        console.log("Post unliked successfully");
-    },
-
-    async addFeedback(postId: number, accountId: number, content: string): Promise<ApiResponse<null>> {
-        console.log("Adding feedback to post API, Post ID:", postId, "Account ID:", accountId, "Content:", content);
-
-        const url = `${baseMappingUrl}/${postId}/add-feedback`;
-        console.log("Request URL:", url);
-
-        const response = await fetch(url, {
-            method: "POST",
-            headers: {
-                "Authorization": `Bearer ${getBearerToken()}`,
-                "Content-Type": "application/json"
-            },
-            body: JSON.stringify({ accountId, content })
-        });
-
-        console.log("Response status:", response.status);
-        if (!response.ok) {
-            const error = await response.json();
-            console.log("Error adding feedback:", error);
-            throw new Error(error?.message || `Error adding feedback to post with ID ${postId}`);
-        }
-
-        const data = await response.json();
-        console.log("Feedback added successfully, response data:", data);
-        return data;
-    },
-
-    async deleteFeedback(postId: number, feedbackId: number): Promise<void> {
-        console.log("Deleting feedback from post API, Post ID:", postId, "Feedback ID:", feedbackId);
-    
-        const url = `${baseMappingUrl}/${postId}/delete-feedback/${feedbackId}`; // Change endpoint to use feedbackId
-        console.log("Request URL:", url);
-    
-        const response = await fetch(url, {
-            method: "DELETE",
-            headers: {
-                "Authorization": `Bearer ${getBearerToken()}`
-            }
-        });
-    
-        console.log("Response status:", response.status);
-        if (!response.ok) {
-            const error = await response.json();
-            console.log("Error deleting feedback:", error);
-            throw new Error(error?.message || `Error deleting feedback from post with ID ${postId}`);
-        }
-    
-        const data = await response.json();
-        console.log("Feedback deleted successfully, response data:", data);
+    log.debug("Response status:", response.status);
+    if (!response.ok) {
+      const error = await response.json();
+      log.error("Error fetching post:", error);
+      throw new Error(
+        error?.message || `Error fetching post with ID ${postId}`,
+      );
     }
-    
-    
+
+    const data = await response.json();
+    log.debug("Fetched post data:", data);
+    return data;
+  },
+
+  // Like a Post
+  likePost: async (
+    postId: number,
+    accountId: number,
+  ): Promise<ApiResponse<null>> => {
+    log.debug("Liking post API, Post ID:", postId, "Account ID:", accountId);
+
+    const url = `${BASE_API_URL}/${postId}/like`;
+    log.debug("Request URL:", url);
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${getBearerToken()}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ accountId }),
+    });
+
+    if (response.status === 500) {
+      const error = await response.json();
+      log.error("Error Code:", response.status);
+      throw new Error(error?.message || `Error liking post with ID ${postId}`);
+    }
+
+    const data = await response.json();
+    log.debug("Post liked successfully, response data:", data);
+    return data;
+  },
+
+  // Unlike a Post
+  unlikePost: async (postId: number, accountId: number): Promise<void> => {
+    log.debug("Unliking post API, Post ID:", postId, "Account ID:", accountId);
+
+    const url = `${BASE_API_URL}/${postId}/unlike/${accountId}`;
+    log.debug("Request URL:", url);
+
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${getBearerToken()}`,
+      },
+    });
+
+    log.debug("Response status:", response.status);
+    if (!response.ok) {
+      const error = await response.json();
+      log.error("Error unliking post:", error);
+      throw new Error(
+        error?.message || `Error unliking post with ID ${postId}`,
+      );
+    }
+
+    log.debug("Post unliked successfully");
+  },
+
+  // Add Feedback to Post
+  addFeedback: async (
+    postId: number,
+    accountId: number,
+    content: string,
+  ): Promise<number> => {
+    log.debug(
+      "Adding feedback to post API, Post ID:",
+      postId,
+      "Account ID:",
+      accountId,
+      "Content:",
+      content,
+    );
+
+    const url = `${BASE_API_URL}/${postId}/add-feedback`;
+    log.debug("Request URL:", url);
+
+    const response = await fetch(url, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${getBearerToken()}`,
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ accountId, content }),
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      log.error("Error adding feedback:", error);
+      throw new Error(
+        error?.message || `Error adding feedback to post with ID ${postId}`,
+      );
+    }
+
+    const data = await response.json();
+    log.debug("Feedback added successfully, response data:", data);
+
+    return data.data.feedbackId;
+  },
+
+  // Delete Feedback from Post
+  deleteFeedback: async (postId: number, feedbackId: number): Promise<void> => {
+    log.debug(
+      "Deleting feedback from post API, Post ID:",
+      postId,
+      "Feedback ID:",
+      feedbackId,
+    );
+
+    const url = `${BASE_API_URL}/${postId}/delete-feedback/${feedbackId}`;
+    log.debug("Request URL:", url);
+
+    const response = await fetch(url, {
+      method: "DELETE",
+      headers: {
+        Authorization: `Bearer ${getBearerToken()}`,
+      },
+    });
+
+    if (!response.ok) {
+      const error = await response.json();
+      log.error("Error deleting feedback:", error);
+      throw new Error(
+        error?.message || `Error deleting feedback from post with ID ${postId}`,
+      );
+    }
+
+    const data = await response.json();
+    log.debug("Feedback deleted successfully, response data:", data);
+  },
 };
 
-
-
 export default postApi;
-
-
