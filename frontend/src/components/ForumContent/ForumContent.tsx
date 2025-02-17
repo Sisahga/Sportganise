290cde95a4779cf4618dfe8e5803b0c5549f3a78
nusrath-z <nusrath.zaman.nz@gmail.com>
2025-02-17 04:23:05 -0500

    refactor(501,502,503): Updated UI for handling like and comment functionality. - Refactor ForumContent and PostContent to update UI for liked, likecount, and comment section. - Added pagination for comments with previous/next buttons in PostsContent - Added comment deletion logic with time limit checks for user-authored comments in PostsContent - Added  logs in PostsContent - Added Badge for author type in PostsContent

--- frontend/src/components/ForumContent/ForumContent.tsx
+++ frontend/src/components/ForumContent/ForumContent.tsx
@@ -1,5 +1,10 @@
 import React, { useState, useEffect, useRef } from "react";
-import { ThumbsUp, MessageSquare,ChevronLeft, ChevronRight } from "lucide-react";
+import {
+  ThumbsUp,
+  MessageCircle,
+  ChevronLeft,
+  ChevronRight,
+} from "lucide-react";
 import { Button } from "@/components/ui/Button";
 import { useNavigate } from "react-router";
 import {
@@ -21,33 +26,38 @@ import BackButton from "../ui/back-button";
 
 import useForumPosts from "@/hooks/useForumPosts";
 import { Badge } from "../ui/badge";
+import { PostDto } from "@/types/forum";
+import { usePostLike } from "@/hooks/usePostLike";
+import { getCookies } from "@/services/cookiesService";
 
 const ForumContent: React.FC = () => {
-
+  const user = getCookies();
   const navigate = useNavigate();
-
-  const [likedposts, setLikedposts] = useState<Set<number>>(new Set());
-  const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage, setPostsPerPage] = useState(10);
-  const [searchTerm, setSearchTerm] = useState<string>("");
-  const [occurrenceDate, setOccurrenceDate] = useState<string | undefined>(undefined);
-  const [type, setType] = useState("");
-  const [selectedLabel] = useState<string>("");
-  const [sortBy, setSortBy] = useState("");
-  const [sortDir, setSortDir] = useState("");
-  const [sortOption, setSortOption] = useState('latest');
-  
   const inputRef = useRef<HTMLInputElement>(null);
 
+  const { posts, loading, error, fetchPostsData, resetFilters } =
+    useForumPosts();
+  const { likePost, unlikePost } = usePostLike();
 
-  const { posts, loading, error, fetchPostsData, resetFilters } = useForumPosts();
+  const [post, setPost] = useState<PostDto[]>([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostsPerPage] = useState(10);
 
+  const [searchTerm, setSearchTerm] = useState<string>("");
+  const [occurrenceDate, setOccurrenceDate] = useState<string | undefined>(
+    undefined,
+  );
 
-  const fetchData = () => {
-    fetchPostsData(
-      searchTerm, occurrenceDate, type, selectedLabel, postsPerPage, currentPage, sortBy, sortDir
-    );
-  };
+  const [type, setType] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [sortDir, setSortDir] = useState("");
+  const [sortOption, setSortOption] = useState("latest");
+
+  useEffect(() => {
+    if (posts) {
+      setPost(posts);
+    }
+  }, [posts]);
 
   useEffect(() => {
     if (inputRef.current && searchTerm != "") {
@@ -56,35 +66,82 @@ const ForumContent: React.FC = () => {
   });
 
   useEffect(() => {
-    const timer = setTimeout(() => {
-      fetchData();
-    }, searchTerm ? 500 : 0);
+    const timer = setTimeout(
+      () => {
+        fetchPostsData(
+          searchTerm,
+          occurrenceDate,
+          type,
+          postsPerPage,
+          currentPage,
+          sortBy,
+          sortDir,
+        );
+      },
+      searchTerm ? 500 : 0,
+    );
 
     return () => clearTimeout(timer);
   }, [searchTerm, currentPage]);
 
-
-  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const searchTerm = e.target.value;
-    setSearchTerm(searchTerm)
-  }
-
-  const handleLike = (postId: number) => {
-    setLikedposts((prev) => {
-      const newLikedposts = new Set(prev);
-      if (newLikedposts.has(postId)) {
-        newLikedposts.delete(postId);
-      } else {
-        newLikedposts.add(postId);
-      }
-      return newLikedposts;
-    });
-  };
-
   const navigatePostDetail = (postId: number) => {
     navigate(`/pages/PostDetailPage`, { state: { postId } });
   };
-  
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const searchTerm = e.target.value;
+    setSearchTerm(searchTerm);
+  };
+
+  const handleLike = (postId: number) => {
+    setPost((prevPosts) =>
+      prevPosts.map((post) => {
+        if (post.postId === postId) {
+          const Liked = !post.liked;
+          const newLikeCount = Liked ? post.likeCount + 1 : post.likeCount - 1;
+
+          if (Liked) {
+            likePost(postId, user.accountId);
+          } else {
+            unlikePost(postId, user.accountId);
+          }
+
+          return {
+            ...post,
+            liked: Liked,
+            likeCount: newLikeCount,
+          };
+        }
+        return post;
+      }),
+    );
+  };
+
+  const handleApplyFilters = () => {
+    fetchPostsData(
+      searchTerm,
+      occurrenceDate,
+      type,
+      postsPerPage,
+      currentPage,
+      sortBy,
+      sortDir,
+    );
+  };
+
+  const handleClearFilters = () => {
+    resetFilters(
+      searchTerm,
+      setOccurrenceDate,
+      setType,
+      setSortBy,
+      setSortOption,
+      setSortDir,
+      setCurrentPage,
+      setPostsPerPage,
+    );
+  };
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -93,23 +150,14 @@ const ForumContent: React.FC = () => {
     return <div>{error}</div>;
   }
 
-  const handleApplyFilters = () => {
-    fetchData();
+  // Utility function to format the occurrence date
+  const formatOccurrenceDate = (dateString: string): string => {
+    const date = new Date(dateString);
+    const hours = date.getHours() % 12 || 12;
+    const minutes = date.getMinutes().toString().padStart(2, "0");
+    const amPm = date.getHours() >= 12 ? "PM" : "AM";
+    return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${hours}:${minutes} ${amPm}`;
   };
-
-  const handleClearFilters = () => {
-    resetFilters(searchTerm, setOccurrenceDate, setType, setSortBy, setSortOption, setSortDir, setCurrentPage, setPostsPerPage);
-  };
-
-
-// Utility function to format the occurrence date
-const formatOccurrenceDate = (dateString: string): string => {
-  const date = new Date(dateString);
-  const hours = date.getHours() % 12 || 12;
-  const minutes = date.getMinutes().toString().padStart(2, '0');
-  const amPm = date.getHours() >= 12 ? 'PM' : 'AM';
-  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()} ${hours}:${minutes} ${amPm}`;
-};
 
   const getBadgeVariant = (programType: string) => {
     switch (programType.toLowerCase()) {
@@ -126,12 +174,12 @@ const formatOccurrenceDate = (dateString: string): string => {
     }
   };
 
-
-
   return (
     <div className="min-h-screen pb-20">
       <BackButton />
-      <h2 className="font-semibold text-3xl text-secondaryColour text-center mb-4">Forum</h2>
+      <h2 className="font-semibold text-3xl text-secondaryColour text-center mb-4">
+        Forum
+      </h2>
 
       {/* Filter Component */}
       <div className="mt-4 mb-2 flex items-center gap-x-1 lg:mx-24">
@@ -158,25 +206,24 @@ const formatOccurrenceDate = (dateString: string): string => {
           className="w-full flex-grow"
           value={searchTerm}
           onChange={handleInputChange}
-
         />
       </div>
 
-      {/* posts Cards */}
+      {/* Posts */}
       <div className="flex flex-col space-y-4 lg:mx-24">
-        {posts.map((post) => (
+        {post.map((post) => (
           <Card
             key={post.postId}
             onClick={() => navigatePostDetail(post.postId)}
             className="cursor-pointer"
           >
             <CardHeader>
-            <CardTitle className="flex">
-  {post.title}
-  <Badge {...getBadgeVariant(post.type)} className="ml-2 h-5">
-    {post.type.toLowerCase()}
-  </Badge>
-</CardTitle>
+              <CardTitle className="flex">
+                {post.title}
+                <Badge {...getBadgeVariant(post.type)} className="ml-2 h-5">
+                  {post.type.toLowerCase()}
+                </Badge>
+              </CardTitle>
 
               <div className="mt-2 text-xs">
                 {formatOccurrenceDate(post.occurrenceDate)}
@@ -188,16 +235,14 @@ const formatOccurrenceDate = (dateString: string): string => {
             <CardFooter className="flex space-x-2">
               <Button
                 variant="outline"
-                className={`flex items-center rounded-full ${likedposts.has(post.postId) ? "text-secondaryColour" : "text-primaryColour"}`}
+                className={`flex items-center rounded-full ${post.liked ? "text-secondaryColour" : "text-primaryColour"}`}
                 onClick={(e) => {
                   e.stopPropagation();
                   handleLike(post.postId);
                 }}
               >
                 <ThumbsUp className="w-4 h-4" />
-                <span>
-                  {post.likeCount + (likedposts.has(post.postId) ? 1 : 0)}
-                </span>
+                <span>{post.likeCount}</span>
               </Button>
 
               <Button
@@ -205,10 +250,12 @@ const formatOccurrenceDate = (dateString: string): string => {
                 className=" flex items-center rounded-full"
                 onClick={(e) => {
                   e.stopPropagation();
-                  navigate(`/pages/PostDetailPage`, { state: { postId: post.postId } });
+                  navigate(`/pages/PostDetailPage`, {
+                    state: { postId: post.postId },
+                  });
                 }}
               >
-                <MessageSquare className="w-4 h-4" />
+                <MessageCircle className="w-4 h-4" />
                 <span>{post.feedbackCount}</span>
               </Button>
             </CardFooter>
@@ -217,14 +264,13 @@ const formatOccurrenceDate = (dateString: string): string => {
       </div>
 
       {/* Pagination */}
-      <div className="mt-4">
+      <div className="mt-4 pb-4">
         <Pagination>
           <PaginationContent>
             <Button
               onClick={() => {
                 if (currentPage > 1) {
                   setCurrentPage((prev) => prev - 1);
-
                 }
               }}
               className="text-primaryColour"
@@ -241,7 +287,6 @@ const formatOccurrenceDate = (dateString: string): string => {
               onClick={() => {
                 if (posts.length >= postsPerPage) {
                   setCurrentPage((prev) => prev + 1);
-
                 }
               }}
               className="text-primaryColour"
@@ -252,7 +297,6 @@ const formatOccurrenceDate = (dateString: string): string => {
             </Button>
           </PaginationContent>
         </Pagination>
-
       </div>
     </div>
   );
