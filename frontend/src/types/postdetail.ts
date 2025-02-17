96e1f786860c15df8afbc4a75b4acb4269990df7
nusrath-z <nusrath.zaman.nz@gmail.com>
2025-02-17 04:15:57 -0500

    refactor(501,502,503): updated frontend API endpoints and response to align with backend

--- frontend/src/types/postdetail.ts
+++ frontend/src/types/postdetail.ts
@@ -1,33 +1,32 @@
 export interface Author {
-    accountId: number;
-    name: string;
-    type: string;
-    pictureUrl: string;
-  }
-  
-  
+  accountId: number;
+  name: string;
+  type: string;
+  pictureUrl: string;
+}
+
 export interface Feedback {
-    feedbackId: number;
-    description: string;
-    author: Author;
-    creationDate: string;
-  }
-  
-  
+  feedbackId: number;
+  description: string;
+  author: Author;
+  creationDate: string;
+}
+
 export interface Post {
-    postId: number;
-    title: string;
-    description: string;
-    type: string;
-    occurenceDate: string;
-    creationDate: string;
-    likeCount: number;
-    feedbackCount: number;
-    feedbackList: Feedback[]; 
-  }
-  
-  export interface ApiResponse<T> {
-    statusCode: number;
-    message: string;
-    data: T;
+  postId: number;
+  title: string;
+  description: string;
+  type: "FUNDRAISER" | "SPECIAL" | "TRAINING" | "TOURNAMENT";
+  occurenceDate: string;
+  creationDate: string;
+  likeCount: number;
+  liked: boolean;
+  feedbackCount: number;
+  feedbackList: Feedback[];
+}
+
+export interface ApiResponse<T> {
+  statusCode: number;
+  message: string;
+  data: T;
 }
