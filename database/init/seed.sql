c2f4620a489fabaac8326f986ffecb1268d685e2
karyennevu <vuongkaryenne@gmail.com>
2025-02-17 23:14:06 -0500

    Changed database table and seed to include a new boolean "shared" for whether or not the plan is shared with the other coaches/admins

--- database/init/seed.sql
+++ database/init/seed.sql
@@ -170,6 +170,7 @@ INSERT INTO feedback (post_id, account_id, content) VALUES
 (2, 3, 'It was mediocre. ');
 
 INSERT INTO training_plan (account_id, doc_url) VALUES
-(1, 'training1.doc'),
-(2, 'training2.doc');
+(1, 'training1.doc', true),
+(1, 'training1.2.doc', false),
+(2, 'training2.doc', true);
 
