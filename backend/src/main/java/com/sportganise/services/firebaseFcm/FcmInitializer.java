fc72416a763bf016fff2a04ac64e56e10cf4cf5d
Sisahga <sisahga@gmail.com>
2025-02-20 17:00:08 -0500

    feat: fcm initializer service class.
    Refs: #643

---
+++ backend/src/main/java/com/sportganise/services/firebaseFcm/FcmInitializer.java
@@ -0,0 +1,34 @@
+package com.sportganise.services.firebaseFcm;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import jakarta.annotation.PostConstruct;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Slf4j
+@Service
+public class FcmInitializer {
+  @Value("${app.firebase-configuration-file}")
+  private String firebaseConfigPath;
+
+  @PostConstruct
+  public void initialize() {
+    try {
+      FirebaseOptions options = FirebaseOptions.builder()
+          .setCredentials(GoogleCredentials.fromStream(new ClassPathResource(firebaseConfigPath).getInputStream()))
+          .build();
+      if (FirebaseApp.getApps().isEmpty()) {
+        FirebaseApp.initializeApp(options);
+        log.info("Firebase application has been initialized.");
+      }
+    } catch (IOException e) {
+      log.error(e.getMessage());
+    }
+  }
+}
