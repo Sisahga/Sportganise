858c636fafee859aae75b4ff965efb0069eae3bf
Sisahga <sisahga@gmail.com>
2025-02-20 20:17:45 -0500

    style: javadoc
    Refs: #643

--- backend/src/main/java/com/sportganise/controllers/NotificationsController.java
+++ backend/src/main/java/com/sportganise/controllers/NotificationsController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Controller class for sending notifications to devices.
+ */
 @RestController
 @RequestMapping("api/notifications")
 public class NotificationsController {
@@ -18,6 +21,12 @@ public class NotificationsController {
     this.fcmService = fcmService;
   }
 
+  /**
+   * Send a notification to a specific device.
+   *
+   * @param request contains the notification title & message, optional topic, & the target device token.
+   * @return a response entity with a success message.
+   */
   @PostMapping("/send")
   public ResponseEntity<ResponseDto<Null>> sendNotification(@RequestBody NotificationRequest request) {
     fcmService.sendMessageToToken(request);
