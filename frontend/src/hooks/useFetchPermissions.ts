1347e1675c13ce9dcb50968461078a1f5c69abb4
nusrath-z <nusrath.zaman.nz@gmail.com>
2025-02-21 21:12:00 -0500

    refactor() - Adding logs using log from loglevel

--- frontend/src/hooks/useFetchPermissions.ts
+++ frontend/src/hooks/useFetchPermissions.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { AccountPermissions } from "@/types/account";
 import accountApi from "@/services/api/accountApi";
+import log from "loglevel";
 
 const useFetchUserPermissions = () => {
   const [data, setData] = useState<AccountPermissions[]>([]);
@@ -10,16 +11,23 @@ const useFetchUserPermissions = () => {
   useEffect(() => {
     const fetchData = async () => {
       try {
+        log.info("Fetching user permissions...");
+
         const result = await accountApi.fetchUserPermissions();
         setData(result);
+
+        log.info("Successfully fetched user permissions", result);
       } catch (err: unknown) {
         if (err instanceof Error) {
           setError(err.message);
+          log.error("Error fetching user permissions:", err.message);
         } else {
           setError("An unexpected error occurred");
+          log.error("Unexpected error fetching user permissions");
         }
       } finally {
         setLoading(false);
+        log.info("Finished fetching user permissions");
       }
     };
 
