89ddc22e31c12b71334346bab9b04a98d252f07a
nusrath-z <nusrath.zaman.nz@gmail.com>
2025-02-21 20:48:56 -0500

    refactor(201)- Changed PopOver to Dialog, modified ui

--- frontend/src/components/UserPermissionContent/UserPermissionContent.tsx
+++ frontend/src/components/UserPermissionContent/UserPermissionContent.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/components/ui/card";
 import { Button } from "@/components/ui/Button";
 import { useNavigate } from "react-router";
-import {FileKey2 } from "lucide-react";
+import { FileKey2} from "lucide-react";
 import {
   Pagination,
   PaginationContent,
@@ -17,11 +17,6 @@ import {
   PaginationNext,
   PaginationPrevious,
 } from "@/components/ui/pagination";
-import {
-  Popover,
-  PopoverContent,
-  PopoverTrigger,
-} from "@/components/ui/popover";
 import {
   Select,
   SelectContent,
@@ -45,14 +40,15 @@ import { AccountPermissions } from "@/types/account";
 import BackButton from "../ui/back-button";
 import useFetchUserPermissions from "@/hooks/useFetchPermissions";
 import useUpdateRole from "@/hooks/useModifyPermissions";
+import { Dialog, DialogContent, DialogTrigger, DialogClose } from "../ui/dialog";
 
 const UserPermissionContent: React.FC = () => {
   const itemsPerPage = 5;
 
   const navigate = useNavigate();
-  
-  const { data, loading, error, setData} = useFetchUserPermissions();
-  const { updateUserRole} = useUpdateRole(data, setData);
+
+  const { data, loading, error, setData } = useFetchUserPermissions();
+  const { updateUserRole } = useUpdateRole(data, setData);
   const { toast } = useToast();
 
   const [currentPage, setCurrentPage] = useState<number>(1);
@@ -73,7 +69,7 @@ const UserPermissionContent: React.FC = () => {
     setCurrentPage(page);
   };
 
-  const openPopover = (user: AccountPermissions) => {
+  const openDialog = (user: AccountPermissions) => {
     setSelectedUser(user);
     setNewRole(user.type);
   };
@@ -99,7 +95,7 @@ const UserPermissionContent: React.FC = () => {
   const confirmRoleChange = () => {
     if (selectedUser) {
       updateUserRole(selectedUser, newRole);
-      setOpenAlertDialog(false); 
+      setOpenAlertDialog(false);
     }
   };
 
@@ -125,18 +121,18 @@ const UserPermissionContent: React.FC = () => {
     <div>
       <BackButton />
       <div className="pb-20 container max-w-2xl mx-auto">
-        <Card className="space-y-8 max-w-3xl mx-auto mt-4 border shadow-md mx-auto max-w-2xl">
+        <Card className="space-y-2 max-w-3xl mx-auto mt-4 border shadow-md mx-auto max-w-2xl">
           <CardHeader>
             <CardTitle className="text-2xl font-bold flex items-center justify-center gap-2 mt-4">
               <FileKey2 className="h-6 w-6" />
               User Permissions
             </CardTitle>
           </CardHeader>
-          <CardContent>
+          <CardContent className="flex flex-col gap-3">
 
             {/* SEARCH BAR --will implement after backend user search is ready */}
 
-            {/* <div className="relative mb-4">
+            {/* <div className="relative ">
               <Input className="mb-2 pl-10" placeholder="Search..." />
               <div className="absolute left-3 top-1/2 transform -translate-y-1/2">
                 <Search size={16} />
@@ -144,87 +140,87 @@ const UserPermissionContent: React.FC = () => {
             </div> */}
 
             {/* Display user data */}
-          
-              <div className="grid grid-cols-1 gap-3">
-                {paginatedData.length > 0 ? (
-                  paginatedData.map((user) => (
-                    <Card key={user.accountId} className="w-full">
-                      <div className="flex items-center gap-2 p-2 pb-0">
-                        <img
-                          src={
-                            user.pictureUrl || "https://via.placeholder.com/150"
-                          }
-                          alt={user.firstName}
-                          className="h-12 w-12 rounded-full"
-                        />
-                        <div>
-                          <p className="text-sm font-bold text-primaryColour">
-                            {user.firstName} {user.lastName}
-                          </p>
-                          <p className="text-xs text-gray">
-                            <span className="font-bold text-gray-500">
-                              Email:
-                            </span>{" "}
-                            {user.email}
-                          </p>
-                          <p className="text-xs text-gray">
-                            <span className="font-bold text-secondaryColour">
-                              Role:
-                            </span>{" "}
-                            {user.type}
-                          </p>
-                        </div>
-                      </div>
-                      <CardFooter className="justify-end p-0 pb-2 pr-2">
-                        <Popover>
-                          <PopoverTrigger asChild>
-                            <Button
-                              className="w-20 h-7"
-                              variant="outline"
-                              size="sm"
-                              onClick={() => openPopover(user)}
-                            >
-                              Modify
-                            </Button>
-                          </PopoverTrigger>
-                          <PopoverContent className="mr-12">
-                            <p className="text-sm text-muted-foreground mb-1">
-                              Modify the permissions for {user.firstName}{" "}
-                              {user.lastName}.
-                            </p>
-                            <Select
-                              value={newRole}
-                              onValueChange={handleRoleChange}
-                            >
-                              <SelectTrigger>
-                                <SelectValue placeholder="Select Role" />
-                              </SelectTrigger>
-                              <SelectContent>
-                                <SelectItem value="ADMIN">Admin</SelectItem>
-                                <SelectItem value="COACH">Coach</SelectItem>
-                                <SelectItem value="PLAYER">Player</SelectItem>
-                                <SelectItem value="GENERAL">General</SelectItem>
-                              </SelectContent>
-                            </Select>
 
-                            <div className="text-center mt-4">
+              {paginatedData.length > 0 ? (
+                paginatedData.map((user) => (
+                  <Card key={user.accountId} className="w-full border-0">
+                    <div className="flex items-center gap-2 p-2 pb-0">
+                      <img
+                        src={
+                          user.pictureUrl || "https://via.placeholder.com/150"
+                        }
+                        alt={user.firstName}
+                        className="h-12 w-12 rounded-full"
+                      />
+                      <div>
+                        <p className="text-sm font-bold text-primaryColour">
+                          {user.firstName} {user.lastName}
+                        </p>
+                        <p className="text-xs text-gray">
+                          <span className="font-bold text-gray-500">
+                            Email:
+                          </span>{" "}
+                          {user.email}
+                        </p>
+                        <p className="text-xs text-gray">
+                          <span className="font-bold text-secondaryColour">
+                            Role:
+                          </span>{" "}
+                          {user.type}
+                        </p>
+                      </div>
+                    </div>
+                    <CardFooter className="justify-end p-0 pb-2 pr-2">
+                      <Dialog>
+                        <DialogTrigger asChild>
+                          <Button
+                            className="w-20 h-7"
+                            variant="outline"
+                            size="sm"
+                            onClick={() => openDialog(user)}
+                          >
+                            Modify
+                          </Button>
+                        </DialogTrigger>
+                        <DialogContent className="rounded-lg w-[90vw]">
+                          <p className="text-sm">
+                            Modify permissions for {user.firstName} {user.lastName}
+                          </p>
+                          <Select value={newRole} onValueChange={handleRoleChange}>
+                            <SelectTrigger>
+                              <SelectValue  placeholder="Select Role" />
+                            </SelectTrigger>
+                            <SelectContent >
+                              <SelectItem value="ADMIN">Admin</SelectItem>
+                              <SelectItem value="COACH">Coach</SelectItem>
+                              <SelectItem value="PLAYER">Player</SelectItem>
+                              <SelectItem value="GENERAL">General</SelectItem>
+                            </SelectContent>
+                          </Select>
+
+
+                          <DialogClose asChild>
+                            <div className="text-center mt-2">
                               <Button
                                 onClick={saveRoleChanges}
-                                variant="outline"
+                                variant="default"
                                 size="sm"
                               >
                                 Save Changes
                               </Button>
                             </div>
-                          </PopoverContent>
-                        </Popover>
-                      </CardFooter>
-                    </Card>
-                  ))
-                ) : (
-                  <div>No users found.</div>
-                )}
-              </div>
+                          </DialogClose>
+
+                        </DialogContent>
+                      </Dialog>
+
+                    </CardFooter>
+                  </Card>
+                ))
+              ) : (
+                <div>No users found.</div>
+              )}
+            {/* </div> */}
           </CardContent>
         </Card>
 
@@ -255,10 +251,11 @@ const UserPermissionContent: React.FC = () => {
 
         {/* Role Change Confirmation Dialog */}
         <AlertDialog open={openAlertDialog} onOpenChange={setOpenAlertDialog}>
-          <AlertDialogContent>
+          <AlertDialogContent className="w-[90vw] rounded-lg">
             <AlertDialogHeader>
               <AlertDialogTitle>Confirm Role Change</AlertDialogTitle>
-              <AlertDialogDescription>
+              <AlertDialogDescription className="text-xs">
+
                 Are you sure you want to change the role for{" "}
                 {selectedUser?.firstName}{" "}
                 {selectedUser?.lastName} to {newRole}?
