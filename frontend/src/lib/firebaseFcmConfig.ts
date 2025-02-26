822dc9549e65d207dcc3589b41f9d373596a5ed2
Sisahga <sisahga@gmail.com>
2025-02-25 22:13:16 -0500

    feat: implemented token retrieval from fcm when user grants permission for push notifications on web.
    Refs: #643

--- frontend/src/lib/firebaseFcmConfig.ts
+++ frontend/src/lib/firebaseFcmConfig.ts
@@ -1,7 +1,8 @@
+/* *** FCM module for Mobile Apps **** */
 import { initializeApp } from "firebase/app";
 import { getMessaging } from "firebase/messaging";
 
-// Base configuration for Firebase
+// Base Firebase app configuration.
 const firebaseConfig = {
   apiKey: "AIzaSyBhikg7e3foOJVa2rXUQV0KcjEfYfI9olM",
   authDomain: "sportganise-notification-f7c40.firebaseapp.com",
@@ -12,9 +13,9 @@ const firebaseConfig = {
   measurementId: "G-QWBV9P289D"
 };
 
-// Initialize Firebase
+// Initialize Firebase.
 const app = initializeApp(firebaseConfig);
-// Initialize Firebase Cloud Messaging (FCM)
+// Initialize Firebase Cloud Messaging (FCM).
 const messaging = getMessaging(app);
 
 export { messaging };
