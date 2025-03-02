583c0a545ed5e34f793a4968ca6dd2143d244589
Kaianinja <cl_manou@live.concordia.ca>
2025-03-02 03:50:29 -0500

    added delete program transactions in repository, for expiry date change

--- backend/src/main/java/com/sportganise/repositories/programsessions/ProgramRecurrenceRepository.java
+++ backend/src/main/java/com/sportganise/repositories/programsessions/ProgramRecurrenceRepository.java
@@ -1,8 +1,23 @@
 package com.sportganise.repositories.programsessions;
 
 import com.sportganise.entities.programsessions.ProgramRecurrence;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.time.ZonedDateTime;
+
 @Repository
-public interface ProgramRecurrenceRepository extends JpaRepository<ProgramRecurrence, Integer> {}
+public interface ProgramRecurrenceRepository extends JpaRepository<ProgramRecurrence, Integer> {
+
+    void deleteProgramRecurrenceByRecurrenceId(Integer recurrenceId);
+
+    @Modifying
+    @Transactional
+    @Query("DELETE FROM ProgramRecurrence rp WHERE rp.programId = :programId AND rp.occurrenceDate <= :expiryDate")
+    void deleteExpiredRecurrences(@Param("expiryDate") ZonedDateTime expiryDate, @Param("programId") Integer programId);
+
+}
