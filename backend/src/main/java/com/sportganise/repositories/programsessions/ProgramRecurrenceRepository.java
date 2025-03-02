7efbfa6fceb55adcc6f9dd77fa2c603daf00b3cc
Kaianinja <cl_manou@live.concordia.ca>
2025-03-02 03:38:09 -0500

    added new method for creating a program recurrence

--- backend/src/main/java/com/sportganise/repositories/programsessions/ProgramRecurrenceRepository.java
+++ backend/src/main/java/com/sportganise/repositories/programsessions/ProgramRecurrenceRepository.java
@@ -1,16 +1,8 @@
 package com.sportganise.repositories.programsessions;
 
-
 import com.sportganise.entities.programsessions.ProgramRecurrence;
-import jakarta.transaction.Transactional;
-import org.springframework.data.jpa.repository.Modifying;
-import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ProgramRecurrenceRepository extends JpaRepository<ProgramRecurrence,Integer> {
-
-}
+public interface ProgramRecurrenceRepository extends JpaRepository<ProgramRecurrence, Integer> {}
