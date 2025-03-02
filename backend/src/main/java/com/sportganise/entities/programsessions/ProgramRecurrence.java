3a62338e4673d148366bd2b8df1639b63ee1801d
Kaianinja <cl_manou@live.concordia.ca>
2025-03-02 03:05:05 -0500

    added entity for program_recurrence

---
+++ backend/src/main/java/com/sportganise/entities/programsessions/ProgramRecurrence.java
@@ -0,0 +1,34 @@
+package com.sportganise.entities.programsessions;
+
+import jakarta.persistence.*;
+
+import java.time.ZonedDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "program_recurrence")
+public class ProgramRecurrence {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "recurrence_id")
+    private Integer recurrenceId;
+
+    @Column(name = "program_id")
+    private Integer programId;
+
+    @Column(name = "occurence_date")
+    private ZonedDateTime occurrenceDate;
+
+    @Column(name = "cancelled")
+    private boolean cancelled;
+}
