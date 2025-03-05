692a36d6202faf33e07720a022e621366adc34ab
Dannick B. Senss <dannickbsenss@gmail.com>
2025-03-05 12:07:44 -0500

    chore: format e2e tests

--- frontend/e2e/sign-up.spec.ts
+++ frontend/e2e/sign-up.spec.ts
@@ -1,36 +1,35 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('should start at login page', async ({ page }) => {
-  await page.goto('/');
+test("should start at login page", async ({ page }) => {
+  await page.goto("/");
 
-  await page.waitForURL('/login')
+  await page.waitForURL("/login");
 
-  await expect(page.getByRole('button', { name: 'Log in' })).toBeVisible();
+  await expect(page.getByRole("button", { name: "Log in" })).toBeVisible();
 });
 
-
-test('should sign up', async ({ page }) => {
+test("should sign up", async ({ page }) => {
   // Navigate to Sign Up page
-  await page.goto('/');
-  await page.waitForURL('/login');
-  await page.getByRole('link', { name: 'Sign Up' }).click();
-  await page.waitForURL('/signup');
+  await page.goto("/");
+  await page.waitForURL("/login");
+  await page.getByRole("link", { name: "Sign Up" }).click();
+  await page.waitForURL("/signup");
 
   // Fill in Sign Up form
   const email = `test-${Date.now()}@onibad.com`;
-  await page.getByRole('textbox', { name: 'Email' }).fill(email);
-  await page.getByRole('textbox', { name: 'Phone' }).fill('1112223333');
-  await page.getByRole('textbox', { name: 'Password' }).fill('Password!123');
-  await page.getByRole('textbox', { name: 'First Name' }).fill('test');
-  await page.getByRole('textbox', { name: 'Last Name' }).fill('test');
-  await page.getByRole('textbox', { name: 'Address' }).fill('test');
-  await page.getByRole('textbox', { name: 'Postal Code' }).fill('test');
-  await page.getByRole('textbox', { name: 'City' }).fill('test');
-  await page.getByRole('textbox', { name: 'Province' }).fill('test');
-  await page.getByRole('textbox', { name: 'Country' }).fill('test');
-  await page.getByRole('button', { name: 'Sign Up' }).click();
+  await page.getByRole("textbox", { name: "Email" }).fill(email);
+  await page.getByRole("textbox", { name: "Phone" }).fill("1112223333");
+  await page.getByRole("textbox", { name: "Password" }).fill("Password!123");
+  await page.getByRole("textbox", { name: "First Name" }).fill("test");
+  await page.getByRole("textbox", { name: "Last Name" }).fill("test");
+  await page.getByRole("textbox", { name: "Address" }).fill("test");
+  await page.getByRole("textbox", { name: "Postal Code" }).fill("test");
+  await page.getByRole("textbox", { name: "City" }).fill("test");
+  await page.getByRole("textbox", { name: "Province" }).fill("test");
+  await page.getByRole("textbox", { name: "Country" }).fill("test");
+  await page.getByRole("button", { name: "Sign Up" }).click();
 
   // Should be redirected to email verification page
-  await page.waitForURL('/verificationcode');
-  await expect(page.getByText('Email Verification')).toBeVisible();
+  await page.waitForURL("/verificationcode");
+  await expect(page.getByText("Email Verification")).toBeVisible();
 });
