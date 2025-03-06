77f61ee1f1d87e7125520b2ca4223eaf06ebb0b9
Sofia Valiante <valiante.sofia@gmail.com>
2025-03-06 10:40:01 -0500

    feat: added constants for location

--- frontend/src/constants/programconstants.ts
+++ frontend/src/constants/programconstants.ts
@@ -1,4 +1,27 @@
+// Program Type Constants
 export const TRAINING = "TRAINING";
 export const SPECIALTRAINING = "SPECIALTRAINING";
 export const FUNDRAISER = "FUNDRAISER";
 export const TOURNAMENT = "TOURNAMENT";
+
+// Program Location Constants
+export const CENTRE_DE_LOISIRS_ST_DENIS = "Centre-de-loisirs-St-Denis";
+export const COLLEGE_DE_MAISONNEUVE = "Collège-de-Maisonnneuve";
+export const TEST_WATER_ROAD = "123 test water rd.";
+export const MAIN_STREET = "123 Main St";
+
+/* const visibilities = [
+    {
+      label: "Public",
+      value: "public",
+    },
+    {
+      label: "Members only",
+      value: "members",
+    },
+    {
+      label: "Private",
+      value: "private",
+    },
+  ] as const;
+  */
