ab7e4d89e3f6da9d63c1e213336ea1bffa5f7fbe
Sofia Valiante <valiante.sofia@gmail.com>
2025-03-06 10:45:33 -0500

    prettier

--- frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
+++ frontend/src/components/CreateTrainingSessionForm/CreateTrainingSessionForm.tsx
@@ -180,7 +180,7 @@ export default function CreateTrainingSessionForm() {
         "programData",
         new Blob([JSON.stringify(programData)], {
           type: "application/json",
-        })
+        }),
       );
       if (values.attachment && values.attachment.length > 0) {
         values.attachment.forEach((file) => {
@@ -197,7 +197,7 @@ export default function CreateTrainingSessionForm() {
       log.info("create", create);
       if (create === null) {
         throw new Error(
-          "Error from useCreateTrainingSession.createTrainingSession!"
+          "Error from useCreateTrainingSession.createTrainingSession!",
         );
       }
       console.log("loading", loading);
@@ -214,7 +214,7 @@ export default function CreateTrainingSessionForm() {
     } catch (err) {
       console.error(
         "Create training session form submission error (error)",
-        err
+        err,
       );
       log.error("Create training session form submission error (error)", err);
       toast({
@@ -294,7 +294,7 @@ export default function CreateTrainingSessionForm() {
                           role="combobox"
                           className={cn(
                             "justify-between",
-                            !field.value && "text-muted-foreground"
+                            !field.value && "text-muted-foreground",
                           )}
                         >
                           {field.value
@@ -324,7 +324,7 @@ export default function CreateTrainingSessionForm() {
                                     "mr-2 h-4 w-4",
                                     type.value === field.value
                                       ? "opacity-100"
-                                      : "opacity-0"
+                                      : "opacity-0",
                                   )}
                                 />
                                 {type.label}
@@ -356,7 +356,7 @@ export default function CreateTrainingSessionForm() {
                           variant={"outline"}
                           className={cn(
                             "pl-3 text-left font-normal",
-                            !field.value && "text-muted-foreground"
+                            !field.value && "text-muted-foreground",
                           )}
                         >
                           {field.value ? (
@@ -403,7 +403,7 @@ export default function CreateTrainingSessionForm() {
                           variant={"outline"}
                           className={cn(
                             "pl-3 text-left font-normal",
-                            !field.value && "text-muted-foreground"
+                            !field.value && "text-muted-foreground",
                           )}
                         >
                           {field.value ? (
@@ -493,12 +493,12 @@ export default function CreateTrainingSessionForm() {
                           role="combobox"
                           className={cn(
                             "justify-between",
-                            !field.value && "text-muted-foreground"
+                            !field.value && "text-muted-foreground",
                           )}
                         >
                           {field.value
                             ? locations.find(
-                                (location) => location.value === field.value
+                                (location) => location.value === field.value,
                               )?.label
                             : "Select location"}
                           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
@@ -524,7 +524,7 @@ export default function CreateTrainingSessionForm() {
                                     "mr-2 h-4 w-4",
                                     location.value === field.value
                                       ? "opacity-100"
-                                      : "opacity-0"
+                                      : "opacity-0",
                                   )}
                                 />
                                 {location.label}
@@ -586,7 +586,7 @@ export default function CreateTrainingSessionForm() {
                           role="combobox"
                           className={cn(
                             !field.value ? "text-muted-foreground" : "",
-                            "justify-between"
+                            "justify-between",
                           )}
                         >
                           {field.value ? field.value : "Select visibility"}
@@ -621,7 +621,7 @@ export default function CreateTrainingSessionForm() {
                                     "mr-2 h-4 w-4",
                                     v.value === field.value
                                       ? "opacity-100"
-                                      : "opacity-0"
+                                      : "opacity-0",
                                   )}
                                 />
                                 {v.label}
@@ -645,7 +645,7 @@ export default function CreateTrainingSessionForm() {
                         <ul className="list-disc pl-5">
                           {selectedMembers.map((memberId) => {
                             const member = members.find(
-                              (m) => m.id === memberId
+                              (m) => m.id === memberId,
                             );
                             return (
                               <li key={memberId}>
@@ -768,7 +768,7 @@ export default function CreateTrainingSessionForm() {
                       {...field}
                       onChange={(e) =>
                         field.onChange(
-                          e.target.value ? Number(e.target.value) : undefined
+                          e.target.value ? Number(e.target.value) : undefined,
                         )
                       }
                     />
