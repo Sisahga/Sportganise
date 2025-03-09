1ee4673247580764edb9bd4e128095062e4b24d8
Kaianinja <cl_manou@live.concordia.ca>
2025-03-09 19:47:10 -0400

    fix

--- backend/src/main/java/com/sportganise/services/programsessions/ProgramService.java
+++ backend/src/main/java/com/sportganise/services/programsessions/ProgramService.java
@@ -472,7 +472,7 @@ public class ProgramService {
         log.debug("PROGRAM ATTACHMENTS COUNT: ", programAttachments.size());
       }
     }
-    return new ProgramDto(updatedProgram, programAttachmentDtos);
+    return new ProgramDto(existingProgram, programAttachmentDtos);
   }
 
   private Program createProgramObject(
