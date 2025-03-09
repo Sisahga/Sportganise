80535ece58e85947de6d41d9633bf6b0f74951b3
Kaianinja <cl_manou@live.concordia.ca>
2025-03-09 18:17:43 -0400

    Modified list of recurrences for a program to return in order of occurence

--- backend/src/main/java/com/sportganise/repositories/programsessions/ProgramRecurrenceRepository.java
+++ backend/src/main/java/com/sportganise/repositories/programsessions/ProgramRecurrenceRepository.java
@@ -23,9 +23,22 @@ public interface ProgramRecurrenceRepository extends JpaRepository<ProgramRecurr
   void deleteExpiredRecurrences(
       @Param("expiryDate") ZonedDateTime expiryDate, @Param("programId") Integer programId);
 
+  @Query("SELECT pr FROM ProgramRecurrence pr WHERE pr.programId = :programId ORDER BY pr.occurrenceDate")
   List<ProgramRecurrence> findProgramRecurrenceByProgramId(Integer programId);
 
   @Query(
       "SELECT pr FROM ProgramRecurrence pr WHERE pr.programId = :programId ORDER BY pr.occurrenceDate DESC")
   Optional<ProgramRecurrence> findLastRecurrenceByProgramId(@Param("programId") Integer programId);
+
+  void deleteProgramRecurrenceByProgramId(Integer programId);
+
+  @Transactional
+  @Modifying
+  @Query("DELETE FROM ProgramRecurrence pr WHERE pr.programId = :programId " +
+          "AND pr.occurrenceDate BETWEEN :firstDate AND :secondDate")
+  void deleteMiddleRecurrences(@Param("programId") Integer programId,
+                               @Param("firstDate") ZonedDateTime firstDate,
+                               @Param("secondDate") ZonedDateTime secondDate);
+
+  List<ProgramRecurrence> findByProgramIdAndOccurrenceDateBetween(Integer programId, ZonedDateTime newStartDate, ZonedDateTime newEndDate);
 }
