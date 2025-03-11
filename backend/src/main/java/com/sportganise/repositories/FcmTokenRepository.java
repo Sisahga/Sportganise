6afb979939dbca169e18189986e4b1404dcd20b3
Sisahga <sisahga@gmail.com>
2025-03-10 20:06:18 -0400

    feat: query for getting fcm tokens related to an account.
    Refs: #400

--- backend/src/main/java/com/sportganise/repositories/FcmTokenRepository.java
+++ backend/src/main/java/com/sportganise/repositories/FcmTokenRepository.java
@@ -1,12 +1,22 @@
 package com.sportganise.repositories;
 
 import com.sportganise.entities.FcmToken;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 /**
  * Repository interface for FcmToken entity.
  */
 @Repository
 public interface FcmTokenRepository extends JpaRepository<FcmToken, String> {
+  @Query("""
+        SELECT token
+        FROM FcmToken
+        WHERE accountId = :accountId
+        """)
+  List<String> findTokensByAccountId(@NotNull Integer accountId);
 }
