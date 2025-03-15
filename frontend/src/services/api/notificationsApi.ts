568114a192f499e8f9acca130b173b835818c9d4
Sisahga <sisahga@gmail.com>
2025-03-15 16:55:59 -0400

    feat: replaced mock data for notification preferences for a user by fetching actual user notification preferences from server.
    Refs: #407

--- frontend/src/services/api/notificationsApi.ts
+++ frontend/src/services/api/notificationsApi.ts
@@ -2,11 +2,27 @@ import { getBearerToken } from "@/services/apiHelper";
 import {
   StoreFcmTokenDto,
   NotificationRequest,
+  NotificationSettings,
 } from "@/types/notifications.ts";
+import ResponseDto from "@/types/response.ts";
 
 const BASE_API_URL = import.meta.env.VITE_API_BASE_URL + "/api/notifications";
 
 const notificationsApi = {
+  getNotificationSettings: async (userId: number) => {
+    const response = await fetch(
+      `${BASE_API_URL}/get-notif-settings/${userId}`,
+      {
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: getBearerToken(),
+        },
+      },
+    );
+    const data: ResponseDto<NotificationSettings> = await response.json();
+    console.log("Notif settings: ", data);
+    return data;
+  },
   sendNotification: async (request: NotificationRequest) => {
     return await fetch(`${BASE_API_URL}/send`, {
       method: "POST",
--- frontend/src/types/notificationSettingOption.ts
+++
@@ -1,5 +0,0 @@
-export interface NotificationOption {
-  id: string;
-  title: string;
-  description: string;
-}
