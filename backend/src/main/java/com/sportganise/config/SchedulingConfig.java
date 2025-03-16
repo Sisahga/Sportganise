f1d99707ae07eca78381adbe0fa98922b66ff1b9
Sisahga <sisahga@gmail.com>
2025-03-16 15:23:13 -0400

    feat: added scheduling configuration file for springboot.
    Refs: #402 #406

---
+++ backend/src/main/java/com/sportganise/config/SchedulingConfig.java
@@ -0,0 +1,12 @@
+package com.sportganise.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
+/**
+ * Configuration class for scheduling tasks.
+ */
+@Configuration
+@EnableScheduling
+public class SchedulingConfig {
+}
