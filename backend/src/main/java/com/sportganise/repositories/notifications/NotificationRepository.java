d4528bd43dedc60ce0d0515070485eab7d15836a
Sisahga <sisahga@gmail.com>
2025-03-16 15:31:27 -0400

    feat: added notification cleanup task to delete messages that are read and over a week old.
    Refs: #402 #406

---
+++ backend/src/main/java/com/sportganise/repositories/notifications/NotificationRepository.java
@@ -0,0 +1,21 @@
+package com.sportganise.repositories.notifications;
+
+import com.sportganise.entities.notifications.Notification;
+import jakarta.transaction.Transactional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.time.ZonedDateTime;
+
+@Repository
+public interface NotificationRepository extends JpaRepository<Notification, Integer> {
+  @Modifying
+  @Transactional
+  @Query("""
+        DELETE FROM Notification n
+        WHERE n.read = true AND n.sentAt < :retentionTime
+        """)
+  int deleteReadNotificationsOlderThanOneWeek(ZonedDateTime retentionTime);
+}
