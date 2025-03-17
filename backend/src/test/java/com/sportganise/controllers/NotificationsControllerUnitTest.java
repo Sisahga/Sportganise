3007d14c74c7000b6596eb9d311fd3fd023bbfa8
Sisahga <sisahga@gmail.com>
2025-03-17 18:59:36 -0400

    style: formatting
    Refs: #402 #404

--- backend/src/test/java/com/sportganise/controllers/NotificationsControllerUnitTest.java
+++ backend/src/test/java/com/sportganise/controllers/NotificationsControllerUnitTest.java
@@ -1,5 +1,11 @@
 package com.sportganise.controllers;
 
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import com.sportganise.services.notifications.FcmService;
 import com.sportganise.services.notifications.NotificationsService;
 import org.junit.jupiter.api.Test;
@@ -13,33 +19,26 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 @WebMvcTest(controllers = NotificationsController.class)
 @ExtendWith(MockitoExtension.class)
 @AutoConfigureMockMvc(addFilters = false)
 public class NotificationsControllerUnitTest {
-  @Autowired
-  private MockMvc mockMvc;
+  @Autowired private MockMvc mockMvc;
 
-  @MockBean
-  private NotificationsService notificationsService;
-  @MockBean
-  private FcmService fcmService;
+  @MockBean private NotificationsService notificationsService;
+  @MockBean private FcmService fcmService;
 
   @Test
   void markAlertsRead_ShouldReturnSuccessMessage() throws Exception {
     Integer userId = 1;
 
-    mockMvc.perform(put("/api/notifications/mark-alerts-read/{userId}", userId)
-                    .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().isOk())
-            .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value()))
-            .andExpect(jsonPath("$.message").value("Alerts marked as read."));
+    mockMvc
+        .perform(
+            put("/api/notifications/mark-alerts-read/{userId}", userId)
+                .contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().isOk())
+        .andExpect(jsonPath("$.statusCode").value(HttpStatus.OK.value()))
+        .andExpect(jsonPath("$.message").value("Alerts marked as read."));
 
     verify(notificationsService, times(1)).markAlertsRead(userId);
   }
