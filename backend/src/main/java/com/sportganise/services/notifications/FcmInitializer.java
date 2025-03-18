b84453397730dcf0cda02bdc4d80afec7f3b4c9f
Sisahga <sisahga@gmail.com>
2025-03-18 15:33:23 -0400

    fix: changed to use FileSystemResources for firebase config path so that it can read files from the VM.

--- backend/src/main/java/com/sportganise/services/notifications/FcmInitializer.java
+++ backend/src/main/java/com/sportganise/services/notifications/FcmInitializer.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.FileSystemResource;
 import org.springframework.stereotype.Service;
 
 /** This class initializes the Firebase application with the configuration file path. */
@@ -17,24 +18,44 @@ public class FcmInitializer {
   @Value("${app.firebase-configuration-file}")
   private String firebaseConfigPath;
 
+  @Value("${environment}")
+  private String env;
+
   /** Initialize the Firebase application with the configuration file path. */
   @PostConstruct
   public void initialize() {
     log.info("Initializing Firebase with config path: {}", firebaseConfigPath);
     try {
-      ClassPathResource resource = new ClassPathResource(firebaseConfigPath);
-      if (!resource.exists()) {
-        log.error("Firebase configuration file not found: {}", firebaseConfigPath);
-        return;
-      }
+      if (env.equals("DEV")) {
+        ClassPathResource resource = new ClassPathResource(firebaseConfigPath);
+        if (!resource.exists()) {
+          log.error("Firebase configuration file not found in dev env: {}", firebaseConfigPath);
+          return;
+        }
 
-      FirebaseOptions options =
-          FirebaseOptions.builder()
-              .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
-              .build();
-      if (FirebaseApp.getApps().isEmpty()) {
-        FirebaseApp.initializeApp(options);
-        log.info("Firebase application has been initialized.");
+        FirebaseOptions options =
+            FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                .build();
+        if (FirebaseApp.getApps().isEmpty()) {
+          FirebaseApp.initializeApp(options);
+          log.info("Firebase application has been initialized in development.");
+        }
+      } else if (env.equals("PROD")) {
+        FileSystemResource resource = new FileSystemResource(firebaseConfigPath);
+        if (!resource.exists()) {
+          log.error("Firebase configuration file not found in prod env: {}", firebaseConfigPath);
+          return;
+        }
+
+        FirebaseOptions options =
+                FirebaseOptions.builder()
+                        .setCredentials(GoogleCredentials.fromStream(resource.getInputStream()))
+                        .build();
+        if (FirebaseApp.getApps().isEmpty()) {
+          FirebaseApp.initializeApp(options);
+          log.info("Firebase application has been initialized in production.");
+        }
       }
     } catch (IOException e) {
       log.error(e.getMessage());
