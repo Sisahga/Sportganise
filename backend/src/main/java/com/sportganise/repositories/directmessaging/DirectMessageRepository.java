8f42017fe0643d658488b150afabb0311d7e17ca
Sisahga <sisahga@gmail.com>
2025-03-20 16:04:39 -0400

    feat: updated backend functionality for querying messages to fetch them dynamically in batches of 30 messages.
    Refs: #699

--- backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageRepository.java
+++ backend/src/main/java/com/sportganise/repositories/directmessaging/DirectMessageRepository.java
@@ -3,6 +3,7 @@ package com.sportganise.repositories.directmessaging;
 import com.sportganise.dto.directmessaging.DmAttachmentDto;
 import com.sportganise.dto.directmessaging.LastMessageDto;
 import com.sportganise.entities.directmessaging.DirectMessage;
+import java.time.ZonedDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
@@ -11,15 +12,26 @@ import org.springframework.stereotype.Repository;
 /** Repository for Direct Message. */
 @Repository
 public interface DirectMessageRepository extends JpaRepository<DirectMessage, Integer> {
+  /**
+   * Get messages by channel ID dynamically. Initially pass NULL for lastSentAt and limit 30 to get
+   * the most recent 30 messages. Subsequently, pass the last sentAt of the last message in the
+   * current list to get the next set
+   *
+   * @param channelId ID of the channel to get messages for.
+   * @param lastSentAt The last sentAt of the last message in the current list.
+   * @param limit The number of messages to fetch.
+   * @return List of messages in the channel.
+   */
   @Query(
       """
           SELECT m
           FROM DirectMessage m
           WHERE m.channelId = :channelId
-          ORDER BY m.sentAt ASC
-          LIMIT 30
+          AND (CAST(:lastSentAt AS timestamp) IS NULL OR m.sentAt < :lastSentAt)
+          ORDER BY m.sentAt DESC
+          LIMIT :limit
          """)
-  List<DirectMessage> getMessagesByChannelId(int channelId);
+  List<DirectMessage> getMessagesByChannelId(int channelId, ZonedDateTime lastSentAt, int limit);
 
   @Query(
       """
@@ -49,7 +61,4 @@ public interface DirectMessageRepository extends JpaRepository<DirectMessage, In
         GROUP BY m.senderId, m.channelId, m.content, m.type
         """)
   LastMessageDto getLastMessageByChannelId(int channelId);
-
-  // TODO: Query to fetch next set of messages based on the last sentAt of the last message in the
-  // current list.
 }
