2b9ef774a3e3fb53289d65b44e2aac38fcffb3a3
Omar Alshanyour <80709119+HelloMeFriend@users.noreply.github.com>
2025-03-21 16:17:09 -0400

    refactor: refactor unused components

--- frontend/src/pages/WaitlistTrainingSessionPage.tsx
+++ frontend/src/pages/WaitlistTrainingSessionPage.tsx
@@ -1,51 +1,34 @@
-  import { useState } from "react";
-  import { HeaderNav } from "@/components/HeaderNav";
-  import { FooterNav } from "@/components/FooterNav";
-  import WaitlistedTrainingSession from "@/components/WaitlistTrainingSession/WaitlistTrainingSession.tsx";
-  import WaitlistDetailsComponent from "@/components/WaitlistTrainingSession/WaitlistDetailsComponent";
-  import { Button } from "@/components/ui/Button";
-  import { Program } from "@/types/trainingSessionDetails";
-  import { useNavigate } from "react-router";
-  import usePrograms from "@/hooks/usePrograms";
-  import { getCookies } from "@/services/cookiesService";
+import { HeaderNav } from "@/components/HeaderNav";
+import { FooterNav } from "@/components/FooterNav";
+import WaitlistedTrainingSession from "@/components/WaitlistTrainingSession/WaitlistTrainingSession.tsx";
+import { Program } from "@/types/trainingSessionDetails";
+import { useNavigate } from "react-router";
+import usePrograms from "@/hooks/usePrograms";
+import { getCookies } from "@/services/cookiesService";
 
-  export default function WaitlistTrainingSessionPage() {
-    const [selectedTraining, setSelectedTraining] = useState<Program | null>(null);
+export default function WaitlistTrainingSessionPage() {
+  const user = getCookies();
+  const navigate = useNavigate();
+  const { programs } = usePrograms(user?.accountId);
 
-    const user = getCookies();
-
-    const navigate = useNavigate();
-    const {programs} = usePrograms(user?.accountId);
-
-    const handleSelectTraining = (program: Program) => {
-      const fullProgram = programs.find(
-        (p) => p.programDetails.programId === program.programDetails.programId
-      );
-      if (fullProgram) {
-        navigate("/pages/ViewTrainingSessionPage", { state: { programDetails: fullProgram.programDetails, attendees: fullProgram.attendees } });
-      } else {
-        navigate("/pages/ViewTrainingSessionPage", { state: { programDetails: program.programDetails, attendees: []} });
-      }
-    };
-
-    return (
-      <div className="flex flex-col">
-        <HeaderNav />
-        <div className="flex-grow mx-4">
-          {selectedTraining ? (
-            <>
-              <Button className="mb-4" onClick={() => setSelectedTraining(null)}>
-                ← Back
-              </Button>
-              <WaitlistDetailsComponent
-                programDetails={selectedTraining.programDetails}
-              />
-            </>
-          ) : (
-            <WaitlistedTrainingSession onSelectTraining={handleSelectTraining} />
-          )}
-        </div>
-        <FooterNav />
-      </div>
+  const handleSelectTraining = (program: Program) => {
+    const fullProgram = programs.find(
+      (p) => p.programDetails.programId === program.programDetails.programId
     );
-  }
+    if (fullProgram) {
+      navigate("/pages/ViewTrainingSessionPage", { state: { programDetails: fullProgram.programDetails, attendees: fullProgram.attendees } });
+    } else {
+      navigate("/pages/ViewTrainingSessionPage", { state: { programDetails: program.programDetails, attendees: [] } });
+    }
+  };
+
+  return (
+    <div className="flex flex-col">
+      <HeaderNav />
+      <div className="flex-grow mx-4">
+        <WaitlistedTrainingSession onSelectTraining={handleSelectTraining} />
+      </div>
+      <FooterNav />
+    </div>
+  );
+}
