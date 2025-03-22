821a8b55e12e7c9b4e4e0da058cc2ffbe17df1ea
Omar Alshanyour <80709119+HelloMeFriend@users.noreply.github.com>
2025-03-22 14:52:41 -0400

    fix issues

--- frontend/src/components/WaitlistTrainingSession/index.tsx
+++ frontend/src/components/WaitlistTrainingSession/index.tsx
@@ -1,5 +1,3 @@
 export { default as WaitlistTrainingSession } from "./WaitlistTrainingSession.tsx";
 export { default as WaitlistedTrainingSessionList } from "./WaitlistedTrainingSessionList.tsx";
 export { default as WaitlistedTrainingSessionCard } from "./WaitlistedTrainingSessionCard.tsx";
-export { default as WaitlistDetailsComponent } from "./WaitlistDetailsComponent.tsx";
-export { default as WaitlistButton } from "./WaitlistButton.tsx";
