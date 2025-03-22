e796dbfa3d4d8fa23a3d965c9683c1470bfb4b35
Omar Alshanyour <80709119+HelloMeFriend@users.noreply.github.com>
2025-03-22 14:52:41 -0400

    fix issues

--- backend/src/main/java/com/sportganise/repositories/programsessions/ProgramParticipantRepository.java
+++ backend/src/main/java/com/sportganise/repositories/programsessions/ProgramParticipantRepository.java
@@ -68,6 +68,16 @@ public interface ProgramParticipantRepository
       """)
   int countConfirmedParticipants(@Param("programId") Integer programId);
 
+  /**
+   * Finds all ProgramParticipant entities for a given account ID.
+   *
+   * @param accountId the account ID to search by.
+   * @return a list of ProgramParticipant entities associated with the given account.
+   */
+  @Query(
+      "SELECT pp FROM ProgramParticipant pp WHERE pp.programParticipantId.accountId = :accountId")
+  List<ProgramParticipant> findByAccountId(@Param("accountId") Integer accountId);
+
   @Query(
       """
       SELECT pp.programParticipantId.accountId
@@ -76,12 +86,4 @@ public interface ProgramParticipantRepository
       AND pp.type = 'Coach'
       """)
   Integer findCoachIdByProgramId(@Param("programId") Integer programId);
-      /**
-     * Finds all ProgramParticipant entities for a given account ID.
-     *
-     * @param accountId the account ID to search by.
-     * @return a list of ProgramParticipant entities associated with the given account.
-     */
-    @Query("SELECT pp FROM ProgramParticipant pp WHERE pp.programParticipantId.accountId = :accountId")
-    List<ProgramParticipant> findByAccountId(@Param("accountId") Integer accountId);
 }
