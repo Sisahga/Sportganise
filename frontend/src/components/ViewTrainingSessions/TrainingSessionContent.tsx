f95f1392b855f1e3f607bca8d5df8fcda4e707cd
Sofia Valiante <valiante.sofia@gmail.com>
2025-03-23 11:31:41 -0400

    fix: display sunday for program content

--- frontend/src/components/ViewTrainingSessions/TrainingSessionContent.tsx
+++ frontend/src/components/ViewTrainingSessions/TrainingSessionContent.tsx
@@ -24,6 +24,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 // Helper function imports
 import { calculateEndTime } from "@/utils/calculateEndTime";
 import { getFileName } from "@/utils/getFileName";
+import { formatDate } from "@/utils/formatDate";
 
 // Data structure for data received from API call
 import { ProgramDetails } from "@/types/trainingSessionDetails";
@@ -140,9 +141,9 @@ const TrainingSessionContent = () => {
             <p className="text-sm text-gray-500">
               {programDetails?.reccurenceDate &&
               programDetails?.frequency !== ONCE
-                ? new Date(programDetails.reccurenceDate).toDateString()
+                ? formatDate(programDetails.reccurenceDate).toDateString()
                 : programDetails?.occurrenceDate
-                  ? new Date(programDetails.occurrenceDate).toDateString()
+                  ? formatDate(programDetails.occurrenceDate).toDateString()
                   : "N/A"}
             </p>
             {programDetails?.expiryDate ? (
