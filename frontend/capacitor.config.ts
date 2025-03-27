eedac901b11041998ec3ba1264d6cc5bc176e108
Sisahga <sisahga@gmail.com>
2025-03-27 16:14:37 -0400

    build: added some capacitor plugins.
    Refs: #695

--- frontend/capacitor.config.ts
+++ frontend/capacitor.config.ts
@@ -3,7 +3,19 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.sportganise.app',
   appName: 'Sportganise',
-  webDir: 'dist'
+  webDir: 'dist',
+  plugins: {
+    Keyboard: {
+      resize: true,
+      style: 'docked'
+    }
+  },
+  server: {
+    allowNavigation: [
+      "https://onibad.sportganise.com",
+      "capacitor://localhost"
+    ]
+  }
 };
 
 export default config;
