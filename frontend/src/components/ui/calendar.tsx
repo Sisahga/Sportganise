8c54ce398f3a732cf661594067750845492575a7
sanaantoun <antounsana@gmail.com>
2025-03-27 21:22:05 -0400

    changed calender cell UI

--- frontend/src/components/ui/calendar.tsx
+++ frontend/src/components/ui/calendar.tsx
@@ -114,14 +114,14 @@ function Calendar({
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        caption_label: "text-md font-semibold",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
           "h-7 w-7 bg-transparent p-0 opacity-50 hover:opacity-100",
         ),
-        nav_button_previous: "absolute left-1",
-        nav_button_next: "absolute right-1",
+        nav_button_previous: "border-primaryColour/70 absolute left-1",
+        nav_button_next: "border-primaryColour/70 absolute right-1",
         table: "w-full border-collapse space-y-1",
         head_row: "flex",
         head_cell:
@@ -131,15 +131,11 @@ function Calendar({
           "relative p-0 text-center text-sm focus-within:relative focus-within:z-20",
           // Range classes:
           props.mode === "range"
-            ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
-            : // Single date classes:
-              "[&:has([aria-selected])]:rounded-md",
-          // Applies a highlight behind the selected day (including outside days if selected)
-          "[&:has([aria-selected])]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-end)]:rounded-r-md",
+          ? "[&:has(>.day-range-end)]:rounded-r-md [&:has(>.day-range-start)]:rounded-l-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md"
+          : "[&:has([aria-selected])]:rounded-md",
         ),
         day: cn(
-          buttonVariants({ variant: "ghost" }),
-          "h-8 w-8 p-0 font-normal aria-selected:opacity-100",
+          "h-8 w-8 p-0 font-normal aria-selected:opacity-100 bg-transparent",
         ),
         day_range_start: "day-range-start",
         day_range_end: "day-range-end",
