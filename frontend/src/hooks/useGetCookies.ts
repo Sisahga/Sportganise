73153557648a730ceed4c9741b1a77060a14e5df
Sisahga <sisahga@gmail.com>
2025-03-29 16:35:37 -0400

    feat: created a hook to fetch cookies, as capacitor fetches them asynchronously on mobile.
    Refs: #695

--- frontend/src/hooks/useGetCookies.ts
+++ frontend/src/hooks/useGetCookies.ts
@@ -1,27 +1,33 @@
-import { getAccountIdCookie, getCookies } from "@/services/cookiesService.ts";
-import { useEffect, useState } from "react";
+import {
+  getAccountIdCookie,
+  getCookies,
+  getEmailCookie,
+} from "@/services/cookiesService.ts";
+import { useCallback, useEffect, useState } from "react";
 import { CookiesDto } from "@/types/auth.ts";
 
 const useGetCookies = () => {
   const [cookies, setCookies] = useState<CookiesDto>();
   const [userId, setUserId] = useState<number>(0);
+  const [email, setEmail] = useState<string>("");
   const [preLoading, setPreLoading] = useState<boolean>(true);
 
-  const fetchCookies = async () => {
+  const fetchCookies = useCallback(async () => {
     const response = await getCookies();
     setCookies(response);
     setUserId(getAccountIdCookie(response));
-  };
+    setEmail(getEmailCookie(response));
+    setPreLoading(false);
+  }, []);
 
   useEffect(() => {
-    fetchCookies().then(() => {
-      setPreLoading(false);
-    });
+    fetchCookies().then((_) => _);
   }, [fetchCookies]);
 
   return {
-    userId,
     cookies,
+    userId,
+    email,
     preLoading,
   };
 };
