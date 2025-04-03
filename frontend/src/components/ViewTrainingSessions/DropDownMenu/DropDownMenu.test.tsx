169a153c4ea5425468837e2da57109478e4b6bb8
Vithu <vithujanan0629@gmail.com>
2025-04-02 20:44:08 -0400

    Format prettier

--- frontend/src/components/ViewTrainingSessions/DropDownMenu/DropDownMenu.test.tsx
+++ frontend/src/components/ViewTrainingSessions/DropDownMenu/DropDownMenu.test.tsx
@@ -153,7 +153,7 @@ describe.skip("DropDownMenuButton component", () => {
         programDetails={programDetails}
         attendees={attendees}
         onRefresh={vi.fn()}
-      />
+      />,
     );
 
     // Verify the trigger button is rendered
@@ -191,7 +191,7 @@ describe.skip("DropDownMenuButton component", () => {
         programDetails={programDetails}
         attendees={attendees}
         onRefresh={vi.fn()}
-      />
+      />,
     );
 
     fireEvent.click(screen.getByLabelText("Add new item"));
@@ -222,7 +222,7 @@ describe.skip("DropDownMenuButton component", () => {
         programDetails={programDetails}
         attendees={attendees}
         onRefresh={vi.fn()}
-      />
+      />,
     );
 
     fireEvent.click(screen.getByLabelText("Add new item"));
@@ -253,7 +253,7 @@ describe.skip("DropDownMenuButton component", () => {
         programDetails={programDetails}
         attendees={attendees}
         onRefresh={vi.fn()}
-      />
+      />,
     );
 
     // Verify the trigger button is rendered
@@ -281,7 +281,7 @@ describe.skip("DropDownMenuButton component", () => {
         programDetails={programDetails}
         attendees={attendees}
         onRefresh={vi.fn()}
-      />
+      />,
     );
 
     // Open the dropdown and click on the 'Postpone Event' item.
@@ -292,7 +292,7 @@ describe.skip("DropDownMenuButton component", () => {
 
     // Verify that the postpone alert dialog is rendered
     const postponeDialogTitle = await screen.findByText(
-      /Would you like to postpone this event\?/i
+      /Would you like to postpone this event\?/i,
     );
     expect(postponeDialogTitle).toBeInTheDocument();
 
@@ -302,7 +302,7 @@ describe.skip("DropDownMenuButton component", () => {
 
     // Check that the postpone confirmation message appears
     const confirmationMessage = await screen.findByText(
-      /You have successfully postponed the event/i
+      /You have successfully postponed the event/i,
     );
     expect(confirmationMessage).toBeInTheDocument();
   });
@@ -329,7 +329,7 @@ describe.skip("DropDownMenuButton component", () => {
         programDetails={programDetails}
         attendees={attendees}
         onRefresh={vi.fn()}
-      />
+      />,
     );
 
     // Open the dropdown and click on the 'RSVP' item.
@@ -340,7 +340,7 @@ describe.skip("DropDownMenuButton component", () => {
 
     // Verify that the RSVP alert dialog is rendered
     const rsvpDialogTitle = await screen.findByText(
-      /Would you like to confirm your presence\?/i
+      /Would you like to confirm your presence\?/i,
     );
     expect(rsvpDialogTitle).toBeInTheDocument();
 
@@ -350,7 +350,7 @@ describe.skip("DropDownMenuButton component", () => {
 
     // Check that the RSVP confirmation message appears
     const confirmationMessage = await screen.findByText(
-      /Your presence is noted/i
+      /Your presence is noted/i,
     );
     expect(confirmationMessage).toBeInTheDocument();
   });
